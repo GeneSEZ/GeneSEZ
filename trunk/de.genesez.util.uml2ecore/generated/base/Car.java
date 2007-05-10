707176e33ada08666d11326421d290a12aceb116
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2007-05-10 16:55:04 +0000

    added 'navigable' to association end

--- trunk/de.genesez.util.uml2ecore/generated/base/Car.java
+++ trunk/de.genesez.util.uml2ecore/generated/base/Car.java
@@ -2,7 +2,7 @@ package base;
 
 /*
 	Car.java
-	generated @ 2007-05-10 18:31:33.203 CEST by dreamer
+	generated @ 2007-05-10 18:52:16.000 CEST by dreamer
  */
 
 /* PROTECTED REGION ID(java.imports._XphC4_41EduRRenReWtq2g) ENABLED START */
@@ -16,7 +16,7 @@ import java.lang.String;
 	<!-- PROTECTED REGION END -->
 	
 	@author		dreamer
-	@version	2007-05-10 18:31:33.203 CEST
+	@version	2007-05-10 18:52:16.000 CEST
  */
 
 
@@ -55,83 +55,11 @@ public class Car  {
 	// -- declare association attributes ----------------------------
 	
 
-	private Wheel mycar;
-
-
-
-	private Wheel owner;
-
 
 	// -- accessores for association attributes ---------------------
 	
 
-	
-	public void insertInMycar(Wheel _in) {
-		
-		if (mycar == _in) {
-			return;
-		}
-		
-		
-		mycar = _in;
-	
-	
-	}
-	
-	
-	public void removeFromMycar	(Wheel _ex) {
-	
-		
-		if (mycar != _ex) {
-			return;
-		}
-		
-		mycar = null;
-	
-	
-	}
-	
-	
-	
-	public Wheel getMycar() {
-		return mycar;
-	}
-	
 
-
-
-	
-	public void insertInOwner(Wheel _in) {
-		
-		if (owner == _in) {
-			return;
-		}
-		
-		
-		owner = _in;
-	
-	
-	}
-	
-	
-	public void removeFromOwner	(Wheel _ex) {
-	
-		
-		if (owner != _ex) {
-			return;
-		}
-		
-		owner = null;
-	
-	
-	}
-	
-	
-	
-	public Wheel getOwner() {
-		return owner;
-	}
-	
 
 
 
