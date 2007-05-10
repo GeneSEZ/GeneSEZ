fdb9c3c59de76356e0448ec7934a4feb51f63147
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2007-05-10 16:45:28 +0000

    added templates for code generation added code generator to workflow added directory for code generation in workflow.properties added useful things to TransUtils.java added template + trans dir to classpath

--- trunk/de.genesez.util.uml2ecore/src/de/genesez/util/TransUtils.java
+++ trunk/de.genesez.util.uml2ecore/src/de/genesez/util/TransUtils.java
@@ -1,10 +1,43 @@
 package de.genesez.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.xmi.XMLResource;
+
+import codegenMm.MClass;
+import codegenMm.MPackage;
 
 public class TransUtils {
 	public static String getXmiId (EObject pObj) {
 		return ((XMLResource)pObj.eResource()).getID(pObj);
+	}
+	
+	public static String getPackagePath(MClass clazz) {
+		StringBuffer p = new StringBuffer();
+		for (MPackage pkg = clazz.getOwningPackage(); pkg != null; pkg = pkg.getNestingPackage()) {
+			p.insert(0, pkg.getName());
+			if (pkg.getNestingPackage() != null) {
+				p.insert(0, ".");
+			}
+		}
+		return p.toString();
+	}
+	
+	/**
+	 * getter for the current timestamp
+	 * @return	string representation from the current timestamp
+	 */
+	public static String getTimestamp() {
+		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS z").format(new Date());
+	}
+	
+	/**
+	 * getter for the current user who logged on
+	 * @return	string representation from the current user
+	 */
+	public static String getUser() {
+		return System.getProperty("user.name", "???");
 	}
 }
