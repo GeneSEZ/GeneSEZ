4f9055fbfb3623ea1a2332f5dfed5b18740e8871
Nico Herbig <nico@nicoherbig.io>
2007-07-11 15:27:12 +0000

    removed because wrong svn repository

--- de.genesez.imsys-pressuresensor/build.bat
+++
@@ -1,6 +0,0 @@
-REM c:\snap1.0.8\bin\preverify.exe -classpath c:\snap1.0.8\bin\snap_rt.jar -nofinalize -d ziel quelle
-
-
-rmdir /S /Q output
-mkdir output
-c:\snap1.0.8\bin\preverify.exe -classpath c:\snap1.0.8\bin\snap_rt.jar -nofinalize -d output bin
