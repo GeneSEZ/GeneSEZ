b9b8c6d04d374284fcd26a0bb6ca18a79344ade8
Nico Herbig <nico@nicoherbig.io>
2007-07-23 15:29:19 +0000

    change mapping

--- de.genesez.templates/mapping/de/genesez/mapping/Mapping.java
+++ de.genesez.templates/mapping/de/genesez/mapping/Mapping.java
@@ -15,14 +15,14 @@ public class Mapping {
 	private static String xmlFile = "..\\de.genesez.templates\\mapping\\de\\genesez\\mapping\\templatemapping.xml";
 
 	
-	public static String getMapTo(String _map, String _mapTo) {
+	public static String typeMap(String _map, String _target) {
 		try {
 			FileReader fr = new FileReader(xmlFile);
 			SAXReader saxReader = new SAXReader(false);
 			Document doc = saxReader.read(new InputSource(fr));
 			fr.close();
 			
-			return getMapToValue(doc, _map, _mapTo);			
+			return getMapToValue(doc, _map, _target);			
 		} catch (Exception e) {
 			System.out.println(e);
 		}
@@ -30,8 +30,8 @@ public class Mapping {
 	}
 	
 	
-	private static String getMapToValue(Document _doc, String _map, String _mapTo) {
-		Element mapping, mapTo;
+	private static String getMapToValue(Document _doc, String _map, String _target) {
+		Element mapping, target;
 		List mappings;
 		
 		mappings = getMappings(getRoot(_doc));
@@ -39,8 +39,8 @@ public class Mapping {
 		while(it.hasNext()) {
 			mapping = (Element)it.next();
 			if (isSearchedMap(getMap(mapping), _map)) {
-				mapTo = getMapTo(getToDefault(mapping), getToOther(mapping, _mapTo));
-				return getValue(mapTo);
+				target = getTarget(getTargetDefault(mapping), getTarget(mapping, _target));
+				return getValue(target);
 			}
 		}
 		return _map;
@@ -62,21 +62,21 @@ public class Mapping {
 	}
 	
 	
-	private static Element getMapTo(Element toDefault, Element toOther) {
-		if (toOther != null) {
-			return toOther;
+	private static Element getTarget(Element targetDefault, Element target) {
+		if (target != null) {
+			return target;
 		} else {
-			return toDefault;
+			return targetDefault;
 		}
 	}
 	
 	
-	private static Element getToDefault(Element mapping) {
+	private static Element getTargetDefault(Element mapping) {
 		return mapping.element("ToDefault");
 	}
 	
 	
-	private static Element getToOther(Element mapping, String mapTo) {
+	private static Element getTarget(Element mapping, String mapTo) {
 		return mapping.element(mapTo);
 	}
 	
