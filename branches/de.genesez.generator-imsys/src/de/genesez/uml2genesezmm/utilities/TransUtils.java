0b271ac92b943aa362bfed5038525f14d2cd35f3
nihe <nihe@a19731f8-c322-0410-8b9b-f98442d78366>
2007-08-01 10:44:35 +0000

    add coments to all methods of the code generator

--- branches/de.genesez.generator-imsys/src/de/genesez/uml2genesezmm/utilities/TransUtils.java
+++ branches/de.genesez.generator-imsys/src/de/genesez/uml2genesezmm/utilities/TransUtils.java
@@ -12,16 +12,30 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.xmi.XMLResource;
 
 
+/**
+ * Helper Class for transformation uml2genesezMM. 
+ * Methods are called from the functions in uml2genesezMM.ext
+ * 
+ * @author geobe, nicher
+ */
 public class TransUtils {
 	
 	
-	// return the xmi id from an element
+	/**
+	 * Return the xmi id form an element
+	 * @param pObj
+	 * @return
+	 */
 	public static String getXmiId (EObject pObj) {
 		return ((XMLResource)pObj.eResource()).getID(pObj);
 	}
 	
 	
-	// return ecore.EJavaObject as list of String
+	/**
+	 * Return ecore.EJavaObject as list of String. 
+	 * @param eobj
+	 * @return
+	 */
 	public static List<String> eToString(Object eobj) {
 		ArrayList<String> retval = new ArrayList<String>();
 		retval.add(eobj.toString());
@@ -29,7 +43,11 @@ public class TransUtils {
 	}
 	
 	
-	// return ecore.EJavaObject as list of String
+	/**
+	 * Return ecore.EJavaObject as list of String.
+	 * @param eobj
+	 * @return
+	 */
 	public static List<String> lToString(org.eclipse.emf.ecore.util.EDataTypeUniqueEList eobj) {
 		ArrayList<String> retval = new ArrayList<String>();
 		retval.addAll(eobj);
@@ -37,7 +55,11 @@ public class TransUtils {
 	}
 	
 	
-	// return the package path from a class or other
+	/**
+	 * Return the package path from a class or other.
+	 * @param clazz
+	 * @return
+	 */
 	public static String getPackagePath(MClassifier clazz) {
 		StringBuffer p = new StringBuffer();
 		for (MPackage pkg = clazz.getOwningPackage(); pkg != null; pkg = pkg.getNestingPackage()) {
@@ -50,19 +72,29 @@ public class TransUtils {
 	}
 	
 
-	// return the full quality name package path from a class or other
+	/**
+	 * Return the full quality name package path from a class or other.
+	 * @param clazz
+	 * @return
+	 */
 	public static String getFQNPackagePath(MClassifier clazz) {
 		return getPackagePath(clazz).replaceAll("\\.", "/");
 	}
 	
 
-	// representation from the current timestamp
+	/**
+	 * Representation from the current timestamp.
+	 * @return
+	 */
 	public static String getTimeStamp() {
 		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS z").format(new Date());
 	}
 	
 	
-	// representation from the current user
+	/**
+	 * Representation from the current user.
+	 * @return
+	 */
 	public static String getUserName() {
 		return System.getProperty("user.name", "unknown user");
 	}
