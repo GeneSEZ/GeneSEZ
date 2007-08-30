a058b4f23651159a65a6fe2ee1b50367a8a57d55
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2007-08-30 20:34:03 +0000

    copied new generator project from branch to trunk

---
+++ trunk/de.genesez.generator-imsys/src/de/genesez/uml2genesezmm/utilities/IOExtensions.java
@@ -0,0 +1,22 @@
+package de.genesez.uml2genesezmm.utilities;
+
+/**
+ * Helper Class for debug output. Methods are called from the functions in
+ * io.ext
+ * 
+ * @author geobe, nicher
+ */
+public class IOExtensions {
+
+	/**
+	 * Print a Object by the toString()-method to the system error print stream.
+	 * 
+	 * @param obj
+	 * @return
+	 */
+	public final static String info(final Object obj) {
+		System.err.println(obj);
+		return obj.toString();
+	}
+
+}
