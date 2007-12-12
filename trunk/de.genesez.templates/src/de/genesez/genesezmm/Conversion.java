80002bcd72604116f0279af11f43a9424d8aacff
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2007-12-12 07:49:51 +0000

    merging + cleaning up + documenting + structure helper methods

--- trunk/de.genesez.templates/src/de/genesez/genesezmm/Conversion.java
+++ trunk/de.genesez.templates/src/de/genesez/genesezmm/Conversion.java
@@ -3,7 +3,6 @@
  */
 package de.genesez.genesezmm;
 
-import genesezMM.MClassifier;
 import genesezMM.MPackage;
 
 /**
@@ -14,19 +13,19 @@ import genesezMM.MPackage;
 public class Conversion {
 	
 	/**
-	 * getter for the package path of a classifier
-	 * @param	clazz		a classifier
-	 * @param	separator	a separator used for the package path
-	 * @return	string representation of the package path
+	 * getter for the full qualified name of a package
+	 * @param	pkg			a package
+	 * @param	separator	a separator used for the full qualified name
+	 * @return	full qualified name of the package
 	 */
-	public static String getPackagePath(MClassifier clazz, String separator) {
-		StringBuffer p = new StringBuffer();
-		for (MPackage pkg = clazz.getOwningPackage(); pkg != null; pkg = pkg.getNestingPackage()) {
-			p.insert(0, pkg.getName());
-			if (pkg.getNestingPackage() != null) {
-				p.insert(0, separator);
+	public static String getFullQualifiedName(MPackage pkg, String separator) {
+		StringBuffer fqn = new StringBuffer();
+		for (MPackage p = pkg; p != null; p = p.getNestingPackage()) {
+			fqn.insert(0, p.getName());
+			if (p.getNestingPackage() != null) {
+				fqn.insert(0, separator);
 			}
 		}
-		return p.toString();
+		return fqn.toString();
 	}
 }
