69f03bed9f70167c99e6aaf96fff8d2d31929e42
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2008-01-08 14:14:21 +0000

    merged release branch 1.1.0 into trunk

--- trunk/de.genesez.example/src-gen/base/Wheel.java
+++ trunk/de.genesez.example/src-gen/base/Wheel.java
@@ -1,6 +1,5 @@
 package base;
 
-
 /* <!-- PROTECTED REGION ID(java.mclassifier.other.import.code_11_5_6340215_1177945943625_542159_161) ENABLED START --> */
 /* <!-- TODO put your imports here --> */
 import java.lang.String;
@@ -13,158 +12,155 @@ import base.parts.Screw;
 
 /* <!-- PROTECTED REGION END --> */
 /**
- * @author nicher
+ * @author dreamer 
  */
 public class Wheel implements Changeable {
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// generated variable, constant and association declarations
+	// ////////////////////////////////////////////////////////////////////////
+	
+	private float diameter;
+	private Set<String> specification = new Set<String>();
+	/**
+	 * variable for association to mycar
+	 */
+	private Car mycar;
+	/**
+	 * variable for association to screw
+	 */
+	private Set<Screw> screw = new Set<Screw>();
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// generated constructor implementations
+	// ////////////////////////////////////////////////////////////////////////
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// generated derived attribute method implementations
+	// ////////////////////////////////////////////////////////////////////////
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// generated association method implementations
+	// ////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * accessor for association to mycar
+	 */
+	public Car getMycar() {
+		return mycar;
+	}
+	
+	/**
+	 * accessor for association to mycar
+	 */
+	public void insertInMycar(Car _in) {
+		if (mycar == _in) {
+			return;
+		}
+		if (mycar != null) {
+			mycar.removeFromWheels(this);
+		}
+		mycar = _in;
+		_in.insertInWheels(this);
+	}
+	
+	/**
+	 * accessor for association to mycar
+	 */
+	public void removeFromMycar(Car _ex) {
+		if (mycar != _ex) {
+			return;
+		}
+		mycar = null;
+		_ex.removeFromWheels(this);
+	}
+	
+	/**
+	 * accessor for association to screw
+	 */
+	public Set<Screw> getScrew() {
+		return java.util.Collections.unmodifiableSet(screw);
+	}
+	
+	/**
+	 * accessor for association to screw
+	 */
+	public void insertInScrew(Screw _in) {
+		if (screw.contains(_in)) {
+			return;
+		}
+		screw.add(_in);
+	}
+	
+	/**
+	 * accessor for association to screw
+	 */
+	public void removeFromScrew(Screw _ex) {
+		if (!screw.contains(_ex)) {
+			return;
+		}
+		screw.remove(_ex);
+	}
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// generated abstract method declaration
+	// ////////////////////////////////////////////////////////////////////////
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// generated normal method implementations
+	// ////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * @param  newWheel  
+	 * @return  
+	 */
+	public Boolean change(Changeable newWheel) {
+		Boolean returnValue = null;
+		/* <!-- PROTECTED REGION ID(java.moperation.implementation.operation.code._11_5_6340215_1179139054625_520706_1) ENABLED START --> */
+		/* <!-- TODO put your own implementation code here --> */
+		/* <!-- PROTECTED REGION END --> */
+		return returnValue;
+	}
+	
+	/**
+	 */
+	public void roll() {
+		
+		/* <!-- PROTECTED REGION ID(java.moperation.implementation.operation.code._11_5_6340215_1179238960437_25222_122) ENABLED START --> */
+		/* <!-- TODO put your own implementation code here --> */
+		/* <!-- PROTECTED REGION END --> */
 
-    // ////////////////////////////////////////////////////////////////////////
-    // generated variable, constant and association declarations
-    // ////////////////////////////////////////////////////////////////////////
-    private float diameter;
-    private Set<String> specification = new HashSet<String>();
-
-    /**
-     * variable for association to mycar
-     */
-    private Car mycar;
-
-    /**
-     * variable for association to screw
-     */
-    private Set<Screw> screw = new HashSet<Screw>();
-
-    // ////////////////////////////////////////////////////////////////////////
-    // generated constructor implementations
-    // ////////////////////////////////////////////////////////////////////////
-
-    // ////////////////////////////////////////////////////////////////////////
-    // generated derived attribute method implementations
-    // ////////////////////////////////////////////////////////////////////////
-
-    // ////////////////////////////////////////////////////////////////////////
-    // generated association method implementations
-    // ////////////////////////////////////////////////////////////////////////
-
-    /**
-     * accessor for association to mycar
-     */
-    public Car getMycar() {
-        return mycar;
-    }
-
-    /**
-     * accessor for association to mycar
-     */
-    public void insertInMycar(Car _in) {
-        if (mycar == _in) {
-            return;
-        }
-        if (mycar != null) {
-            mycar.removeFromWheels(this);
-        }
-        mycar = _in;
-        _in.insertInWheels(this);
-    }
-
-    /**
-     * accessor for association to mycar
-     */
-    public void removeFromMycar(Car _ex) {
-        if (mycar != _ex) {
-            return;
-        }
-        mycar = null;
-        _ex.removeFromWheels(this);
-    }
-
-    /**
-     * accessor for association to screw
-     */
-    public Set<Screw> getScrew() {
-        return Collections.unmodifiableSet(screw);
-    }
-
-    /**
-     * accessor for association to screw
-     */
-    public void insertInScrew(Screw _in) {
-        if (screw.contains(_in)) {
-            return;
-        }
-        screw.add(_in);
-    }
-
-    /**
-     * accessor for association to screw
-     */
-    public void removeFromScrew(Screw _ex) {
-        if (!screw.contains(_ex)) {
-            return;
-        }
-        screw.remove(_ex);
-    }
-
-    // ////////////////////////////////////////////////////////////////////////
-    // generated abstract method declaration
-    // ////////////////////////////////////////////////////////////////////////
-
-    // ////////////////////////////////////////////////////////////////////////
-    // generated normal method implementations
-    // ////////////////////////////////////////////////////////////////////////
-
-    /**
-     * @param  newWheel
-     * @return
-     */
-    public boolean change(Changeable newWheel) {
-        Boolean returnValue = null;
-
-        /* <!-- PROTECTED REGION ID(java.moperation.implementation.operation.code._11_5_6340215_1179139054625_520706_1) ENABLED START --> */
-        /* <!-- TODO put your own implementation code here --> */
-
-        /* <!-- PROTECTED REGION END --> */
-        return returnValue;
-    }
-
-    /**
-     */
-    public void roll() {
-
-        /* <!-- PROTECTED REGION ID(java.moperation.implementation.operation.code._11_5_6340215_1179238960437_25222_122) ENABLED START --> */
-        /* <!-- TODO put your own implementation code here --> */
-
-        /* <!-- PROTECTED REGION END --> */
-    }
-
-    // ////////////////////////////////////////////////////////////////////////
-    // generated getter and setter method implementations
-    // ////////////////////////////////////////////////////////////////////////
-    public float getDiameter() {
-        return diameter;
-    }
-
-    public void setDiameter(float _diameter) {
-        diameter = _diameter;
-    }
-
-    public Set getSpecification() {
-        return Collections.unmodifiableSet(specification);
-    }
-
-    public void addToSpecification(String _in) {
-        specification.add(_in);
-    }
-
-    public void removeFromSpecification(String _ex) {
-        specification.remove(_ex);
-    }
-
-    // ////////////////////////////////////////////////////////////////////////
-    // own implementations
-    // ////////////////////////////////////////////////////////////////////////
-
-    /* <!-- PROTECTED REGION ID(java.mclassifier.implementation.owncode.code._11_5_6340215_1177945943625_542159_161) ENABLED START --> */
-    /* <!-- TODO put your own implementation code here --> */
-
-    /* <!-- PROTECTED REGION END --> */
+	}
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// generated getter and setter method implementations
+	// ////////////////////////////////////////////////////////////////////////
+	
+	public float getDiameter() {
+		return diameter;
+	}
+	
+	public void setDiameter(float _diameter) {
+		diameter = _diameter;
+	}
+	
+	public Set getSpecification() {
+		return java.util.Collections.unmodifiableSet(specification);
+	}
+	
+	public void addToSpecification(String _in) {
+		specification.add(_in);
+	}
+	
+	public void removeFromSpecification(String _ex) {
+		specification.remove(_ex);
+	}
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// own implementations
+	// ////////////////////////////////////////////////////////////////////////
+	
+	/* <!-- PROTECTED REGION ID(java.mclassifier.implementation.owncode.code._11_5_6340215_1177945943625_542159_161) ENABLED START --> */
+	/* <!-- TODO put your own implementation code here --> */
+	/* <!-- PROTECTED REGION END --> */
 }
