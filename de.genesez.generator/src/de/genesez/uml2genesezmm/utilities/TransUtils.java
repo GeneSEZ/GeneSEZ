37df81ffe2e1cac6e8289c76c32f1b33d0f5a3fe
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2008-01-18 08:06:15 +0000

    completely rewritten uml2genesezmm script, added support for external classes

--- de.genesez.generator/src/de/genesez/uml2genesezmm/utilities/TransUtils.java
+++ de.genesez.generator/src/de/genesez/uml2genesezmm/utilities/TransUtils.java
@@ -62,78 +62,78 @@ public class TransUtils {
 	}
 	
 	
-	/**
-	 * Return the package path from a class or other.
-	 * @param clazz
-	 * @return
-	 */
-	public static String getPackagePath(MClassifier clazz) {
-		StringBuffer p = new StringBuffer();
-		for (MPackage pkg = clazz.getOwningPackage(); pkg != null; pkg = pkg.getNestingPackage()) {
-			p.insert(0, pkg.getName());
-			if (pkg.getNestingPackage() != null) {
-				p.insert(0, ".");
-			}
-		}
-		return p.toString();
-	}
+//	/**
+//	 * Return the package path from a class or other.
+//	 * @param clazz
+//	 * @return
+//	 */
+//	public static String getPackagePath(MClassifier clazz) {
+//		StringBuffer p = new StringBuffer();
+//		for (MPackage pkg = clazz.getOwningPackage(); pkg != null; pkg = pkg.getNestingPackage()) {
+//			p.insert(0, pkg.getName());
+//			if (pkg.getNestingPackage() != null) {
+//				p.insert(0, ".");
+//			}
+//		}
+//		return p.toString();
+//	}
 	
 
-	/**
-	 * Return the full quality name package path from a class or other.
-	 * @param clazz
-	 * @return
-	 */
-	public static String getFQNPackagePath(MClassifier clazz) {
-		return getPackagePath(clazz).replaceAll("\\.", "/");
-	}
+//	/**
+//	 * Return the full quality name package path from a class or other.
+//	 * @param clazz
+//	 * @return
+//	 */
+//	public static String getFQNPackagePath(MClassifier clazz) {
+//		return getPackagePath(clazz).replaceAll("\\.", "/");
+//	}
 	
 
-	/**
-	 * Representation from the current timestamp.
-	 * @return
-	 */
-	public static String getTimeStamp() {
-		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS z").format(new Date());
-	}
+//	/**
+//	 * Representation from the current timestamp.
+//	 * @return
+//	 */
+//	public static String getTimeStamp() {
+//		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS z").format(new Date());
+//	}
 	
 	
-	/**
-	 * Representation from the current user.
-	 * @return
-	 */
-	public static String getUserName() {
-		return System.getProperty("user.name", "unknown user");
-	}
+//	/**
+//	 * Representation from the current user.
+//	 * @return
+//	 */
+//	public static String getUserName() {
+//		return System.getProperty("user.name", "unknown user");
+//	}
 	
 	
-	/**
-	 * Filters a list of packages with a comma or semicolon separated list of package names.
-	 * The package names can contain white space. The separation between the package names
-	 * can also contain white space for better looking.
-	 * 
-	 * @param packages			a list with uml packages, found in a model
-	 * @param excludePackages	comma or semicolon separated list with package names to exclude
-	 * @return					given list with uml packages without the excluded packages
-	 */
-	public static List<Package> rejectExcludedPackages(List<Package> packages, String excludePackages) {
-		// can't remove packages from the list while iterating over it, so store all packages to remove here
-		List<Package> toRemove = new ArrayList<Package>();
-		// note that some packages can contain the blank (' ') in their name!
-		StringTokenizer st = new StringTokenizer(excludePackages, ",;");
-		while (st.hasMoreTokens()) {
-			String s = st.nextToken().trim();
-			Iterator<Package> it = packages.iterator();
-			while (it.hasNext()) {
-				Package p = it.next();
-				if (p.getName().equals(s)) {
-					toRemove.add(p);
-				}
-			}
-		}
-		// remove all excluded packages
-		List<Package> toReturn = new ArrayList<Package>(packages);
-		toReturn.removeAll(toRemove);
-		return toReturn;
-	}
+//	/**
+//	 * Filters a list of packages with a comma or semicolon separated list of package names.
+//	 * The package names can contain white space. The separation between the package names
+//	 * can also contain white space for better looking.
+//	 * 
+//	 * @param packages			a list with uml packages, found in a model
+//	 * @param excludePackages	comma or semicolon separated list with package names to exclude
+//	 * @return					given list with uml packages without the excluded packages
+//	 */
+//	public static List<Package> rejectPackages(List<Package> packages, String excludePackages) {
+//		// can't remove packages from the list while iterating over it, so store all packages to remove here
+//		List<Package> toRemove = new ArrayList<Package>();
+//		// note that some packages can contain the blank (' ') in their name!
+//		StringTokenizer st = new StringTokenizer(excludePackages, ",;");
+//		while (st.hasMoreTokens()) {
+//			String s = st.nextToken().trim();
+//			Iterator<Package> it = packages.iterator();
+//			while (it.hasNext()) {
+//				Package p = it.next();
+//				if (p.getName().equals(s)) {
+//					toRemove.add(p);
+//				}
+//			}
+//		}
+//		// remove all excluded packages
+//		List<Package> toReturn = new ArrayList<Package>(packages);
+//		toReturn.removeAll(toRemove);
+//		return toReturn;
+//	}
 }
