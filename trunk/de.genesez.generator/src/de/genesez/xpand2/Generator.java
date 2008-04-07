d706e818895483e7c599bff319718b4c8daab9da
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2008-04-07 15:30:50 +0000

    modified to use genesez generator with genesez typemapping

--- trunk/de.genesez.generator/src/de/genesez/xpand2/Generator.java
+++ trunk/de.genesez.generator/src/de/genesez/xpand2/Generator.java
@@ -8,9 +8,8 @@ public class Generator extends org.openarchitectureware.xpand2.Generator {
 	public Generator() {
 		super();
 	}
-		
+	
 	public void setTypeMappingFile(final String typeMappingFile) {
 		TypeMapper.initTypeMapper(typeMappingFile);
     }
-
 }
