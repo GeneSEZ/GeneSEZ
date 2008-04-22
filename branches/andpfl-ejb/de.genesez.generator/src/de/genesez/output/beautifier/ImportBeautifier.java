fce223518ac714e8505a578a4995ebb5639c7bca
andpfl <andpfl@a19731f8-c322-0410-8b9b-f98442d78366>
2008-04-22 14:05:17 +0000

    own copy because of planned changes in trunk-version

---
+++ branches/andpfl-ejb/de.genesez.generator/src/de/genesez/output/beautifier/ImportBeautifier.java
@@ -0,0 +1,62 @@
+package de.genesez.output.beautifier;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openarchitectureware.xpand2.output.PostProcessor;
+
+/**
+ * Basic abstract class for concrete import beautifier.
+ * 
+ * @author nicher
+ */
+public abstract class ImportBeautifier implements PostProcessor {
+
+	/**
+	 * Logger instance.
+	 */
+	protected final Log log = LogFactory.getLog(getClass());
+
+	/**
+	 * Singleton import formatter instance.
+	 */
+	private ImportFormatter importFormatter = null;
+
+	/**
+	 * Properties for import formatter
+	 */
+	private Properties options = null;
+
+	/**
+	 * 
+	 */
+	public ImportBeautifier() {
+	}
+
+	/**
+	 * @return the options for the import formatter.
+	 */
+	public Properties getOptions() {
+		return options;
+	}
+
+	/**
+	 * @param options
+	 *            for the import formatter.
+	 */
+	public void setOptions(Properties options) {
+		this.options = options;
+	}
+
+	/**
+	 * Returns an instance of the import formatter.
+	 * 
+	 * @return a preconfigured instance of the import formatter.
+	 */
+	protected ImportFormatter getImportFormatter() {
+		if (importFormatter == null) {
+			importFormatter = new ImportFormatter(options);
+		}
+		return importFormatter;
+	}
+
+}
---
+++ branches/andpfl-ejb/de.genesez.generator/src/de/genesez/xpand2/Generator.java
@@ -0,0 +1,15 @@
+package de.genesez.xpand2;
+
+import de.genesez.common.typemapping.TypeMapper;
+
+
+public class Generator extends org.openarchitectureware.xpand2.Generator {
+	
+	public Generator() {
+		super();
+	}
+	
+	public void setTypeMappingFile(final String typeMappingFile) {
+		TypeMapper.initTypeMapper(typeMappingFile);
+    }
+}
