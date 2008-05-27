2e51ed08a982a86d37832ffcebc328bb1ca489eb
Nico Herbig <nico@nicoherbig.io>
2008-05-27 07:29:18 +0000

    documentation of the import formatter

--- de.genesez.platforms.common/src/de/genesez/platforms/common/m2t/ImportBeautifier.java
+++ de.genesez.platforms.common/src/de/genesez/platforms/common/m2t/ImportBeautifier.java
@@ -1,56 +1,66 @@
 package de.genesez.platforms.common.m2t;
 
+import java.util.Properties;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openarchitectureware.xpand2.output.PostProcessor;
 
 /**
- * Basic abstract class for concrete import beautifier.
+ * This class provides a skeletal implementation for concrete import beautifier.
+ * The task of an import beautifier is the detection and reduction of double
+ * import statements. Furthermore, this class implements the PostProcessor
+ * interface which allows to define a concrete import beautifier as a beautifier
+ * attribute in the Xpand workflow component.
  * 
- * @author nicher
+ * @author nihe
+ * @date 2008-02-24
  */
 public abstract class ImportBeautifier implements PostProcessor {
-
+	
 	/**
-	 * Logger instance.
+	 * Using log4j-mechanism for logging errors.
 	 */
 	protected final Log log = LogFactory.getLog(getClass());
-
+	
 	/**
 	 * Singleton import formatter instance.
 	 */
 	private ImportFormatter importFormatter = null;
-
+	
 	/**
-	 * Properties for import formatter
+	 * Stores the settings for the import formatter.
 	 */
 	private Properties options = null;
-
+	
 	/**
-	 * 
+	 * Sole constructor which does nothing.
 	 */
 	public ImportBeautifier() {
 	}
-
+	
 	/**
-	 * @return the options for the import formatter.
+	 * Getter for the settings of the import formatter.
+	 * 
+	 * @return the settings as Properties class
 	 */
 	public Properties getOptions() {
 		return options;
 	}
-
+	
 	/**
-	 * @param options
-	 *            for the import formatter.
+	 * Setter for the settings of the import formatter.
+	 * 
+	 * @param options the settings as Properties class
 	 */
 	public void setOptions(Properties options) {
 		this.options = options;
 	}
-
+	
 	/**
-	 * Returns an instance of the import formatter.
+	 * Sole method to get the preconfigured import formatter.
 	 * 
-	 * @return a preconfigured instance of the import formatter.
+	 * @return singleton import formatter instance
 	 */
 	protected ImportFormatter getImportFormatter() {
 		if (importFormatter == null) {
@@ -58,5 +68,5 @@ public abstract class ImportBeautifier implements PostProcessor {
 		}
 		return importFormatter;
 	}
-
+	
 }
--- de.genesez.platforms.common/src/de/genesez/platforms/common/m2t/ImportFormatter.java
+++ de.genesez.platforms.common/src/de/genesez/platforms/common/m2t/ImportFormatter.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Properties;
 import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.regex.Matcher;
@@ -15,104 +16,119 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * @author nicher
+ * The task of this class is to perform the the detection and reduction of
+ * double import statements. The detection of import statements is solved via
+ * regular expressions.
  * 
+ * @author nihe
+ * @date 2008-02-24
+ * @todo support more than one import section
  */
 public class ImportFormatter {
-
+	
 	/**
-	 * Logger instance.
+	 * Using log4j-mechanism for logging errors.
 	 */
 	private final Log log = LogFactory.getLog(getClass());
-
+	
 	/**
-	 * Properties for import formatter.
+	 * Stores the settings of the import formatter.
 	 */
 	private Properties options = null;
-
+	
 	/**
-	 * A list of lines from a text file.
+	 * The list of lines from a file.
 	 */
 	private List<String> fileLines = null;
-
+	
 	/**
-	 * A set of import statements.
+	 * The set of non-double import statements.
 	 */
 	private Set<String> importStatements = null;
-
+	
 	/**
-	 * First position of an import statement.
+	 * The first position of an import statement.
 	 */
 	private Integer firstImportStatement = null;
-
+	
 	/**
-	 * Last position of an import statement.
+	 * The last position of an import statement.
 	 */
 	private Integer lastImportStatement = null;
-
+	
 	/**
-	 * @param options
-	 *            for the import formatter
+	 * Sole constructor which sets the settings of the import formatter. Actual
+	 * the property keys "de.genesez.importformatter.delim" and
+	 * "de.genesez.importformatter.regex" are supported. The
+	 * "de.genesez.importformatter.delim" key has the task to split lines. The
+	 * "de.genesez.importformatter.regex" key is used to find import statements.
+	 * 
+	 * @param options the settings as Properties class
 	 */
 	public ImportFormatter(Properties options) {
 		this.options = options;
-
+		
 		if (!options.containsKey("de.genesez.importformatter.delim"))
-			log
-					.error("The property: 'de.genesez.importformatter.delim' isn't set.");
+			log.error("The property: 'de.genesez.importformatter.delim' isn't set.");
 		if (!options.containsKey("de.genesez.importformatter.regex"))
-			log
-					.error("The property: 'de.genesez.importformatter.regex' isn't set.");
+			log.error("The property: 'de.genesez.importformatter.regex' isn't set.");
 	}
-
+	
 	/**
-	 * @param edit
-	 * @return a formatted content of a file.
+	 * This method performs the detection and reduction of double import
+	 * statements and returns the modified file content.
+	 * 
+	 * @param edit the file content
+	 * @return the formatted file content
 	 */
 	public String format(String edit) {
 		String returnValue = null;
-
-		String delim = options.getValue("de.genesez.importformatter.delim");
-		String regex = options.getValue("de.genesez.importformatter.regex");
-
+		
+		String delim = options.getProperty("de.genesez.importformatter.delim");
+		String regex = options.getProperty("de.genesez.importformatter.regex");
+		
 		splitStringInLines(edit, delim);
 		findImports(regex);
-
+		
 		returnValue = generateModifiedFileContent();
-
+		
 		return returnValue;
 	}
-
+	
 	/**
-	 * @param edit
-	 * @param delim
-	 *            The delimiter to break lines.
+	 * This method splits the lines by means of the property key
+	 * "de.genesez.importformatter.delim".
+	 * 
+	 * @param edit the file content
+	 * @param delim the delimiter to split lines
 	 * @return
 	 */
 	private List<String> splitStringInLines(String edit, String delim) {
 		fileLines = new LinkedList<String>();
 		StringTokenizer st = new StringTokenizer(edit, delim);
-
+		
 		while (st.hasMoreTokens()) {
 			fileLines.add(st.nextToken());
 		}
 		return Collections.unmodifiableList(fileLines);
 	}
-
+	
 	/**
-	 * @param regex
-	 *            The regular expression.
-	 * @return
+	 * This method searches import statements by means of the property key
+	 * "de.genesez.importformatter.regex".
+	 * 
+	 * @param regex the regular expression
+	 * @return the detected import lines as set of strings
 	 */
 	private Set<String> findImports(String regex) {
 		importStatements = new HashSet<String>();
-
+		
 		firstImportStatement = -1;
 		lastImportStatement = -1;
-
+		
 		Matcher matcher = null;
 		String actLine = null;
-
+		
 		for (int i = 0; i < fileLines.size(); i++) {
 			actLine = fileLines.get(i);
 			matcher = initRegex(regex, actLine);
@@ -130,44 +146,44 @@ public class ImportFormatter {
 		}
 		return Collections.unmodifiableSet(importStatements);
 	}
-
+	
 	/**
-	 * @return the modified context.
+	 * This method create the new file content without double import statement.
+	 * 
+	 * @return the modified context
 	 */
 	private String generateModifiedFileContent() {
 		String returnValue = "";
-
+		
 		// text before the first import statement
 		for (int i = 0; i < firstImportStatement; i++) {
 			returnValue += fileLines.get(i);
 		}
-
+		
 		// the import statements
 		Iterator<String> it = importStatements.iterator();
 		while (it.hasNext()) {
 			returnValue += it.next();
 		}
-
+		
 		// text after the last import statement
 		for (int i = lastImportStatement + 1; i < fileLines.size(); i++) {
 			returnValue += fileLines.get(i);
 		}
-
+		
 		return returnValue;
 	}
-
+	
 	/**
 	 * Translate regular expression and instantiate a matcher object.
 	 * 
-	 * @param regex
-	 *            The regular expression.
-	 * @param input
-	 *            The input string.
-	 * @return a matcher object for parsing.
+	 * @param regex the regular expression
+	 * @param input the input string
+	 * @return the matcher object for parsing
 	 */
 	private Matcher initRegex(String regex, String input) {
 		Matcher matcher = null;
-
+		
 		try {
 			Pattern pattern = Pattern.compile(regex);
 			matcher = pattern.matcher(input);
@@ -176,5 +192,5 @@ public class ImportFormatter {
 		}
 		return matcher;
 	}
-
+	
 }
