02624bc7e2f6affc1527b95ee067c5ae4b93a873
Nico Herbig <nico@nicoherbig.io>
2008-05-28 11:15:56 +0000

    documentation of the type mapping system

--- de.genesez.platforms.common/src/de/genesez/platforms/common/typemapping/TypeMapper.java
+++ de.genesez.platforms.common/src/de/genesez/platforms/common/typemapping/TypeMapper.java
@@ -13,25 +13,46 @@ import de.genesez.platforms.common.typemapping.mappingtypes.CollectionMappingTyp
 import de.genesez.platforms.common.typemapping.mappingtypes.ExternalMappingType;
 import de.genesez.platforms.common.typemapping.mappingtypes.PrimitiveMappingType;
 
+/**
+ * This class is the main class of the type mapping system which handles the
+ * whole transformations.
+ * 
+ * @author nihe
+ * @date 2008-02-18
+ */
 public class TypeMapper {
 	
-	/** Singleton type mapper instance. */
+	/**
+	 * Singleton type mapper instance.
+	 */
 	private static TypeMapper typeMapper = null;
 	
-	/** Logger instance. */
+	/**
+	 * Using log4j-mechanism for error logging.
+	 */
 	private final Log log = LogFactory.getLog(getClass());
-
-	/** type mapping manager for type mappings */
+	
+	/**
+	 * Type mapping manager for collection type mappings.
+	 */
 	private ITypeMappingManager<CollectionMappingType> collectionTypeManager = null;
+	
+	/**
+	 * Type mapping manager for primitive type mappings.
+	 */
 	private ITypeMappingManager<PrimitiveMappingType> primitiveTypeManager = null;
+	
+	/**
+	 * Type mapping manager for external type mappings.
+	 */
 	private ITypeMappingManager<ExternalMappingType> externalTypeManager = null;
 	
-	/*
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	 * constructor 
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	/**
+	 * Sole constructor which starts the parsing of the type mapping file.
+	 * 
+	 * @param typeMappingFile the type mapping file in which the type
+	 *            transformations are stored
 	 */
-	
 	private TypeMapper(String typeMappingFile) {
 		log.info("Type Mapping: type mapping initialized with file " + typeMappingFile);
 		
@@ -41,22 +62,33 @@ public class TypeMapper {
 		
 		new CollectionTypeXMLReader().parseTypeMappingFile(typeMappingFile);
 		new PrimitiveTypeXMLReader().parseTypeMappingFile(typeMappingFile);
-		new ExternalTypeXMLReader().parseTypeMappingFile(typeMappingFile);	
+		new ExternalTypeXMLReader().parseTypeMappingFile(typeMappingFile);
 	}
 	
-	/*
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	 * normal methods for type mapping
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	/**
+	 * This method returns if any entry exists the transformed value else the
+	 * basic value for this type. Attention!!! This method is not for collection
+	 * types. Please use the getMappingType(...) function.
+	 * 
+	 * @param type the type which shall be transformed
+	 * @return the transformed or standard type as String representation
 	 */
-	
 	public String getMappingName(MType type) {
 		return getMappingName(type, null);
 	}
 	
+	/**
+	 * This method returns if any entry exists the transformed value else the
+	 * basic value for this type. Attention!!! This method is not for collection
+	 * types. Please use the getMappingType(...) function.
+	 * 
+	 * @param type the type which shall be transformed
+	 * @param context the implementation context
+	 * @return the transformed or standard type as String representation
+	 */
 	public String getMappingName(MType type, String context) {
 		String returnValue = null;
-
+		
 		if (type instanceof MPrimitiveType)
 			returnValue = getPrimitiveTypeMapping(((MPrimitiveType) type).getName(), context);
 		else if (type instanceof MClassifier)
@@ -71,32 +103,48 @@ public class TypeMapper {
 		}
 		return returnValue;
 	}
-
+	
+	/**
+	 * This method returns a language specific collection by means of the unique
+	 * and ordered statements. If no matching value is detected, then the
+	 * default collection type will be used. Attention!!! This method is only
+	 * for collection types.
+	 * 
+	 * @param unique true if the collection shall be unique otherwise false
+	 * @param ordered true if the collection shall be ordered otherwise false
+	 * @return the transformed or standard collection type as String
+	 *         representation
+	 */
 	public String getMappingType(Boolean unique, Boolean ordered) {
 		return getMappingType(unique, ordered, null);
 	}
 	
+	/**
+	 * This method returns a language specific collection by means of the unique
+	 * and ordered statements. If no matching value is detected, then the
+	 * default collection type will be used. Attention!!! This method is only
+	 * for collection types.
+	 * 
+	 * @param unique true if the collection shall be unique otherwise false
+	 * @param ordered true if the collection shall be ordered otherwise false
+	 * @param context the implementation context
+	 * @return the transformed or standard collection type as String
+	 *         representation
+	 */
 	public String getMappingType(Boolean unique, Boolean ordered, String context) {
 		return getCollectionTypeMapping(unique, ordered, context);
 	}
 	
-	/*
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	 * private methods for type mapping manager
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	/**
+	 * Private method which checks if a context is given or not. Furthermore, it
+	 * returns the language specific primitive type or the model type if no
+	 * transformation settings exists.
+	 * 
+	 * @param map the type which shall be transformed (model type)
+	 * @param context the implementation context or null
+	 * @return the language specific type or the model type as String
+	 *         representation
 	 */
-	
-	private String getCollectionTypeMapping(Boolean unique, Boolean ordered, String context) {
-		CollectionMappingType cmt = new CollectionMappingType(unique, ordered, "");
-		String to = null;
-		
-		if (context == null)
-			to = collectionTypeManager.getTypeMapping(cmt);
-		else
-			to = collectionTypeManager.getTypeMapping(cmt, context);
-		return decideValue("", to);
-	}
-	
 	private String getPrimitiveTypeMapping(String map, String context) {
 		PrimitiveMappingType pmt = new PrimitiveMappingType(map, "");
 		String to = null;
@@ -108,6 +156,38 @@ public class TypeMapper {
 		return decideValue(map, to);
 	}
 	
+	/**
+	 * Private method which checks if a context is given or not. Furthermore, it
+	 * returns the language specific collection type or a empty string if no
+	 * transformation settings exists.
+	 * 
+	 * @param unique true if the collection shall be unique otherwise false
+	 * @param ordered true if the collection shall be ordered otherwise false
+	 * @param context the implementation context or null
+	 * @return the language specific type or a empty string as String
+	 *         representation
+	 */
+	private String getCollectionTypeMapping(Boolean unique, Boolean ordered, String context) {
+		CollectionMappingType cmt = new CollectionMappingType(unique, ordered, "");
+		String to = null;
+		
+		if (context == null)
+			to = collectionTypeManager.getTypeMapping(cmt);
+		else
+			to = collectionTypeManager.getTypeMapping(cmt, context);
+		return decideValue("", to);
+	}
+	
+	/**
+	 * Private method which checks if a context is given or not. Furthermore, it
+	 * returns the language specific external type or the model type if no
+	 * transformation settings exists.
+	 * 
+	 * @param map the type which shall be transformed (model type)
+	 * @param context the implementation context or null
+	 * @return the language specific type or the model type as String
+	 *         representation
+	 */
 	private String getExternalTypeMapping(String map, String context) {
 		ExternalMappingType emt = new ExternalMappingType(map, "");
 		String to = null;
@@ -118,7 +198,16 @@ public class TypeMapper {
 			to = externalTypeManager.getTypeMapping(emt, context);
 		return decideValue(map, to);
 	}
-		
+	
+	/**
+	 * This method decide if the model type or the language specific type will
+	 * be returned. If the language specific type is null, then the model type
+	 * will be returned, otherwise the language specific type.
+	 * 
+	 * @param map the type which shall be transformed (model type)
+	 * @param to the language specific type
+	 * @return the model type or the language specific type
+	 */
 	private String decideValue(String map, String to) {
 		if (to != null) {
 			return to;
@@ -127,19 +216,25 @@ public class TypeMapper {
 		}
 	}
 	
-	/*
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	 * singleton instance creator and init method for TypeMapper class
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	/**
+	 * This static method initializes the type mapper. Please call this function
+	 * before the beginning of the model to text transformation.
+	 * 
+	 * @param typeMappingFile the type mapping file
+	 * @return singleton type mapper instance
 	 */
-	
 	public static TypeMapper initTypeMapper(String typeMappingFile) {
 		if (typeMapper == null) {
 			typeMapper = new TypeMapper(typeMappingFile);
 		}
 		return typeMapper;
 	}
-
+	
+	/**
+	 * Sole method to get the preconfigured type mapper.
+	 * 
+	 * @return singleton type mapper instance
+	 */
 	public static TypeMapper getInstance() {
 		if (typeMapper == null) {
 			typeMapper = new TypeMapper("../de.genesez.platforms.common/script/de/genesez/common/typemapping/typeMapping.xml");
@@ -147,23 +242,26 @@ public class TypeMapper {
 		return typeMapper;
 	}
 	
-	public static void main(String[] args) {
-		TypeMapper.getInstance();
-	}
-
-	/*
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	 * singleton instance creator and init method for TypeMapper class
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	/**
+	 * This class parse a type mapping file and search for collection types.
+	 * 
+	 * @author nihe
 	 */
-	
 	private class CollectionTypeXMLReader extends XMLTypeMappingReader<CollectionMappingType> {
-
+		
+		/**
+		 * Sole constructor which tells the super constructor to search for
+		 * collection types.
+		 */
 		public CollectionTypeXMLReader() {
 			super(collectionTypeManager, "collectionTypes");
 		}
 		
-		@Override
+		/**
+		 * This method creates a new collection type.
+		 * 
+		 * @see de.genesez.platforms.common.typemapping.XMLTypeMappingReader#buildMappingType(org.dom4j.Element)
+		 */
 		protected CollectionMappingType buildMappingType(Element mappingElement) {
 			CollectionMappingType cmt = null;
 			
@@ -180,13 +278,26 @@ public class TypeMapper {
 		
 	}
 	
+	/**
+	 * This class parse a type mapping file and search for primitive types.
+	 * 
+	 * @author nihe
+	 */
 	private class PrimitiveTypeXMLReader extends XMLTypeMappingReader<PrimitiveMappingType> {
-
+		
+		/**
+		 * Sole constructor which tells the super constructor to search for
+		 * primitive types.
+		 */
 		public PrimitiveTypeXMLReader() {
 			super(primitiveTypeManager, "primitiveTypes");
 		}
 		
-		@Override
+		/**
+		 * This method creates a new primitive type.
+		 * 
+		 * @see de.genesez.platforms.common.typemapping.XMLTypeMappingReader#buildMappingType(org.dom4j.Element)
+		 */
 		protected PrimitiveMappingType buildMappingType(Element mappingElement) {
 			PrimitiveMappingType pmt = null;
 			
@@ -198,13 +309,26 @@ public class TypeMapper {
 		
 	}
 	
+	/**
+	 * This class parse a type mapping file and search for external types.
+	 * 
+	 * @author nihe
+	 */
 	private class ExternalTypeXMLReader extends XMLTypeMappingReader<ExternalMappingType> {
-
+		
+		/**
+		 * Sole constructor which tells the super constructor to search for
+		 * external types.
+		 */
 		public ExternalTypeXMLReader() {
 			super(externalTypeManager, "externalTypes");
 		}
 		
-		@Override
+		/**
+		 * This method creates a new external type.
+		 * 
+		 * @see de.genesez.platforms.common.typemapping.XMLTypeMappingReader#buildMappingType(org.dom4j.Element)
+		 */
 		protected ExternalMappingType buildMappingType(Element mappingElement) {
 			ExternalMappingType emt = null;
 			
--- de.genesez.platforms.common/src/de/genesez/platforms/common/typemapping/XMLTypeMappingReader.java
+++ de.genesez.platforms.common/src/de/genesez/platforms/common/typemapping/XMLTypeMappingReader.java
@@ -16,56 +16,77 @@ import org.xml.sax.InputSource;
 
 import de.genesez.platforms.common.typemapping.mappingtypes.MappingType;
 
-public abstract class XMLTypeMappingReader<T extends MappingType> {
+/**
+ * This class parse a XML type mapping file.
+ * 
+ * @author nihe
+ * @param <T> the specific mapping type
+ * @todo enable validation
+ * @date 2008-02-18
+ */
+public abstract class XMLTypeMappingReader<T extends MappingType> implements ITypeMappingReader {
 	
-	/** Logger instance. */
-	private final Log log = LogFactory.getLog(getClass());
-
-	private ITypeMappingManager<T> manager = null;
-	private String nameOfMappingTypes = null;
-		
-	/*
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	 * constructor 
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	/**
+	 * Using log4j-mechanism for error logging.
 	 */
-		
+	private final Log log = LogFactory.getLog(getClass());
+	
+	/**
+	 * The type mapping manager which manages all type mappings.
+	 */
+	private ITypeMappingManager<T> manager = null;
+	
+	/**
+	 * The name of the specific type mapping type, e.g. collectionTypes,
+	 * primitiveTypes and so on.
+	 */
+	private String nameOfMappingTypes = null;
+	
+	/**
+	 * Sole constructor which set the type mapping manager and the type of
+	 * mapping types, e.g. primitiveTypes, externalTypes and so on.
+	 * 
+	 * @param manager
+	 * @param nameOfMappingTypes
+	 */
 	public XMLTypeMappingReader(ITypeMappingManager<T> manager, String nameOfMappingTypes) {
 		this.manager = manager;
 		this.nameOfMappingTypes = nameOfMappingTypes;
 	}
 	
-	/*
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	 * abstract methods 
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	/**
+	 * This abstract method creates a new specific type mapping type, e.g.
+	 * primitiveTypes, externalTypes and so on.
+	 * 
+	 * @param mappingElement the parent element (<mappingElement> ...
+	 *            </mappingElement>)
+	 * @return a new specific type mapping type which stores the transformation
+	 *         settings
 	 */
-	
 	protected abstract T buildMappingType(Element mappingElement);
 	
-	/*
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	 * public methods 
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	/**
+	 * This method starts the parsing of a XML type mapping file.
+	 * 
+	 * @param typeMappingFile the XML type mapping file
 	 */
-	
 	public void parseTypeMappingFile(String typeMappingFile) {
 		Element root = null;
 		Element header = null;
 		
-		String superMappingFile = "";
+		String superTypeMappingFile = "";
 		
 		try {
 			log.info("Type Mapping: type mapping with file " + typeMappingFile);
 			root = getXMLRoot(getXMLDocument(typeMappingFile));
 			header = getHeader(root);
-
+			
 			storeMappingElementsInManager(root);
 			
-			superMappingFile = header.elementText("superMappingFile");
-			if (!superMappingFile.equals("")) {
-				log.info("Type Mapping: recursion of type mapping with file " + superMappingFile); 
-				parseTypeMappingFile(superMappingFile);
+			superTypeMappingFile = header.elementText("superMappingFile");
+			if (!superTypeMappingFile.equals("")) {
+				log.info("Type Mapping: recursion of type mapping with file " + superTypeMappingFile);
+				parseTypeMappingFile(superTypeMappingFile);
 			}
 		} catch (Exception ex) {
 			log.error("Type Mapping: " + ex.getStackTrace());
@@ -74,18 +95,31 @@ public abstract class XMLTypeMappingReader<T extends MappingType> {
 		
 	}
 	
+	/**
+	 * Getter for the default language specific type.
+	 * 
+	 * @param mappingElement the parent element (<mappingElement> ...
+	 *            </mappingElement>)
+	 * @return the default language specific type as String representation
+	 */
 	protected String getDefaultType(Element mappingElement) {
 		return mappingElement.elementText("defaultType");
 	}
-		
+	
+	/**
+	 * This method stores all type mappings from a specific mapping type in
+	 * the type mapping manager.
+	 * 
+	 * @param root the parent element (<typeMapping> ... </typeMapping>)
+	 */
 	private void storeMappingElementsInManager(Element root) {
 		List mappingElements = null;
 		Element mappingElement = null;
 		
 		mappingElements = getMappingElements(root);
 		Iterator it = mappingElements.iterator();
-		while(it.hasNext()) {
-			mappingElement = (Element)it.next();
+		while (it.hasNext()) {
+			mappingElement = (Element) it.next();
 			T mappingType = buildMappingType(mappingElement);
 			storeContextTypes(mappingType, mappingElement);
 			// store type mapping in manager
@@ -93,6 +127,15 @@ public abstract class XMLTypeMappingReader<T extends MappingType> {
 		}
 	}
 	
+	/**
+	 * This method is called from the storeMappingElementsInManager(Element
+	 * root) method to store all context specific type mappings in the type
+	 * mapping manager.
+	 * 
+	 * @param mappingType the specific mapping type
+	 * @param mappingElement the parent element (<mappingElement> ...
+	 *            </mappingElement>)
+	 */
 	private void storeContextTypes(T mappingType, Element mappingElement) {
 		List contextTypes = null;
 		Element contextType = null;
@@ -101,40 +144,58 @@ public abstract class XMLTypeMappingReader<T extends MappingType> {
 		
 		contextTypes = getContextTypes(mappingElement);
 		Iterator it = contextTypes.iterator();
-		while(it.hasNext()) {
-			contextType = (Element)it.next();
+		while (it.hasNext()) {
+			contextType = (Element) it.next();
 			context = contextType.elementText("context");
 			type = contextType.elementText("type");
 			mappingType.addContextType(context, type);
 		}
 	}
-		
+	
+	/**
+	 * Returns the header of a XML type mapping file. The header includes the
+	 * information about the super type mapping file.
+	 * 
+	 * @param root the parent element (<typeMapping> ... </typeMapping>)
+	 * @return the header element (<header> ... </header>)
+	 */
 	private Element getHeader(Element root) {
 		return root.element("header");
 	}
 	
+	/**
+	 * This method returns a list of type mappings based on a specific type
+	 * mapping type, e.g. primitiveTypes, externalTypes and so on.
+	 * 
+	 * @param root the parent element (<typeMapping> ... </typeMapping>)
+	 * @return a list of type mappings as List representation
+	 */
 	private List getMappingElements(Element root) {
 		return root.element(nameOfMappingTypes).elements("mappingElement");
 	}
 	
+	/**
+	 * This method returns a list of context type mapping elements (<contextType>
+	 * ... </contextType>).
+	 * 
+	 * @param mappingElement the parent element (<mappingElement> ...
+	 *            </mappingElement>)
+	 * @return a list of context type mappings as List representation
+	 */
 	private List getContextTypes(Element mappingElement) {
 		return mappingElement.elements("contextType");
 	}
 	
-	/*
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	 *  
-	 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	 */
-	
 	/**
-	 * Returns the xml document.
+	 * Create a Document class instance from a XML type mapping file.
 	 * 
-	 * @param xmlFile
-	 * @return
-	 * @throws FileNotFoundException
-	 * @throws DocumentException
-	 * @throws IOException
+	 * @param typeMappingFile the XML type mapping file
+	 * @return the XML type mapping file as Document class representation
+	 * @throws FileNotFoundException this exception is thrown when the file
+	 *             doesn't exists
+	 * @throws DocumentException this exception is thrown when an error occurs
+	 *             during the processing of a DOM4J document
+	 * @throws IOException this exception is thrown when any I/O error occurs
 	 */
 	private Document getXMLDocument(String typeMappingFile) throws FileNotFoundException, DocumentException, IOException {
 		FileReader fileReader = new FileReader(typeMappingFile);
@@ -146,11 +207,11 @@ public abstract class XMLTypeMappingReader<T extends MappingType> {
 	}
 	
 	/**
-	 * Returns the root element of this mapping file
-	 * [typeMapping]...[/typeMapping].
+	 * This method returns the root element (<typeMapping>...</typeMapping>)
+	 * of a XML mapping file.
 	 * 
-	 * @param doc
-	 * @return
+	 * @param doc the XML type mapping file as Document class representation
+	 * @return the root element of a file
 	 */
 	private Element getXMLRoot(Document doc) {
 		return doc.getRootElement();
