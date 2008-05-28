1852acb054900131bd8789a0c39f08a32d27b073
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2008-05-28 12:43:11 +0000

    branched for GeneSEZ Release 1.5

---
+++ branches/genesez_RELEASE_1_5_0/de.genesez.platforms.common/src/de/genesez/platforms/common/workflow/xpand2/Generator.java
@@ -0,0 +1,27 @@
+package de.genesez.platforms.common.workflow.xpand2;
+
+import de.genesez.platforms.common.typemapping.TypeMapper;
+
+/**
+ * GeneSEZ specific Generator Class supporting the GeneSEZ type mapping
+ * 
+ * @author	nihe
+ * @date	2008-02-19
+ */
+public class Generator extends org.openarchitectureware.xpand2.Generator {
+	
+	/**
+	 * constructs the GeneSEZ Generator
+	 */
+	public Generator() {
+		super();
+	}
+	
+	/**
+	 * setter for the type mapping file
+	 * @param	typeMappingFile		name of the file used by the generator for type mapping
+	 */
+	public void setTypeMappingFile(final String typeMappingFile) {
+		TypeMapper.initTypeMapper(typeMappingFile);
+    }
+}
