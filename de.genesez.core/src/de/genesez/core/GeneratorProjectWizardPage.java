995851ae0e3e5e25a1247169fd9a5cbad310aac6
gerbe <gerbe@a19731f8-c322-0410-8b9b-f98442d78366>
2008-06-01 17:58:56 +0000

    - PlatformProjectWizard is working now :-) - Fixed some mistakes in build.xml

--- de.genesez.core/src/de/genesez/core/GeneratorProjectWizardPage.java
+++ de.genesez.core/src/de/genesez/core/GeneratorProjectWizardPage.java
@@ -1,10 +1,17 @@
 package de.genesez.core;
 
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.widgets.Composite;
 
 public class GeneratorProjectWizardPage extends WizardPage {
 
+	public GeneratorProjectWizardPage(ISelection selection) {
+		super("wizardPage");
+		this.setTitle("GeneSEZ Generator Project");
+		this.setDescription("Generates a new Generator Project based on GeneSEZ Framework");
+	}
+	
 	public void createControl(Composite parent) {
 		// TODO Auto-generated method stub
 
--- de.genesez.core/src/de/genesez/core/PlatformProjectWizardPage.java
+++ de.genesez.core/src/de/genesez/core/PlatformProjectWizardPage.java
@@ -1,13 +1,69 @@
 package de.genesez.core;
 
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 public class PlatformProjectWizardPage extends WizardPage {
 
+	private Text projectText;
+
+	public PlatformProjectWizardPage(ISelection selection) {
+		super("wizardPage");
+		this.setTitle("GeneSEZ Platform");
+		this.setDescription("Generates a new Platform Project for the GeneSEZ Framework");
+	}
+	
 	public void createControl(Composite parent) {
 		// TODO Auto-generated method stub
 
+		Composite container = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		container.setLayout(layout);
+		layout.numColumns = 2;
+		layout.verticalSpacing = 9;
+
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		Label label = new Label(container, SWT.NULL);
+		label.setText("&Project name:");
+
+		this.projectText = new Text(container, SWT.BORDER | SWT.SINGLE);
+		gd = new GridData(GridData.FILL_HORIZONTAL);
+		this.projectText.setLayoutData(gd);
+		this.projectText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				dialogChanged();
+			}
+		});
+
+		this.initialize();
+		this.dialogChanged();
+		this.setControl(container);
+	}
+
+	public String getProjectName() {
+		return this.projectText.getText();
+	}
+	
+	private void dialogChanged() {
+
+		if ( 0 == this.projectText.getText().length() ) {
+			this.setErrorMessage("Project name must be specified");
+			return;
+		}
+		setErrorMessage(null);
+		setPageComplete(true);
+	}
+
+	private void initialize() {
+		this.projectText.setText("de.genesez.platforms.newplatform");
 	}
 
 }
