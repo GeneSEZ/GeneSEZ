aaa9bb49c6da8a7c4522560b3d8b5dea1235581e
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2008-06-04 11:06:30 +0000

    added the newly generated files

---
+++ de.genesez.example.java.car/src-gen/de/genesez/example/java/car/base/package-info.java
@@ -0,0 +1,7 @@
+
+/**
+ * <p>
+ *   contains all the basic and essential things to describe a <b><i>Car</i></b>
+ * </p>
+ */
+package de.genesez.example.java.car.base;
---
+++ de.genesez.example.java.car/src-gen/de/genesez/example/java/car/base/parts/package-info.java
@@ -0,0 +1,13 @@
+
+/**
+ * <p>
+ *   contains some additional things to some parts or pieces of a <b><i>Car</i></b> 
+ *   together
+ * </p>
+ * 
+ * 
+ * 
+ * 
+ * der geschachtelte kommentar
+ */
+package de.genesez.example.java.car.base.parts;
