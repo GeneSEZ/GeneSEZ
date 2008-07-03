5de6b3aad1ac25b1c15beac0e6b929dc29133ae9
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2008-07-03 14:18:05 +0000

    enhanced the model a bit, added association handling jar, regenerated using templates with new association handling

--- branches/RELEASE_1_6/de.genesez.example.java.car/src-gen/de/genesez/example/java/car/base/package-info.java
+++ branches/RELEASE_1_6/de.genesez.example.java.car/src-gen/de/genesez/example/java/car/base/package-info.java
@@ -1,1 +1,1 @@
-/** * <p> *   contains all the basic and essential things to describe a <b><i>Car</i></b> * </p> */package de.genesez.example.java.car.base;
+/** * contains all the basic and essential things to describe a Car */package de.genesez.example.java.car.base;
--- branches/RELEASE_1_6/de.genesez.example.java.car/src-gen/de/genesez/example/java/car/base/parts/package-info.java
+++ branches/RELEASE_1_6/de.genesez.example.java.car/src-gen/de/genesez/example/java/car/base/parts/package-info.java
@@ -1,1 +1,1 @@
-/** * <p> *   contains some additional things to some parts or pieces of a <b><i>Car</i></b>  *   together * </p> *  *  *  *  * der geschachtelte kommentar */package de.genesez.example.java.car.base.parts;
+/** * contains some additional things to some parts or pieces of a Car together *  *  *  * der geschachtelte kommentar */package de.genesez.example.java.car.base.parts;
