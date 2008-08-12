efddd104da9acee176654a91cd0576a56c579f5b
gerbe <gerbe@a19731f8-c322-0410-8b9b-f98442d78366>
2008-08-12 15:47:58 +0000

    TransformHelper is no longer static anymore; introduced support for ControlFlows

--- de.genesez.adapter.ea/src/de/genesez/adapter/ea/ElementFactory.java
+++ de.genesez.adapter.ea/src/de/genesez/adapter/ea/ElementFactory.java
@@ -4,13 +4,13 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.uml2.uml.Action;
 import org.eclipse.uml2.uml.Activity;
+import org.eclipse.uml2.uml.ActivityEdge;
 import org.eclipse.uml2.uml.ActivityFinalNode;
 import org.eclipse.uml2.uml.Actor;
+import org.eclipse.uml2.uml.ControlFlow;
 import org.eclipse.uml2.uml.FlowFinalNode;
 import org.eclipse.uml2.uml.InitialNode;
-import org.eclipse.uml2.uml.NamedElement;
 import org.eclipse.uml2.uml.Package;
-import org.eclipse.uml2.uml.Stereotype;
 import org.eclipse.uml2.uml.UMLFactory;
 import org.eclipse.uml2.uml.UseCase;
 
@@ -23,20 +23,22 @@ public class ElementFactory {
 	 * @param e	the children element
 	 * @param p	the UML package
 	 */
-	public static void create(org.sparx.Element e, Package p) {
+	public static void create(org.sparx.Element e, Package p, TransformHelper _t) {
 		log.info("Create Element for " + e.GetName());
-		
+
+		TransformHelper t = new TransformHelper();
+
 		if ( e.GetType().equals("Actor") ) {
 			log.info("Element is an Actor");
 			Actor actor = UMLFactory.eINSTANCE.createActor();
 			actor.setPackage(p);
-			TransformHelper.transform(e, actor);
+			t.transform(e, actor);
 		}
 		else if ( e.GetType().equals("UseCase") ) {
 			log.info("Element is a UseCase");
 			UseCase usecase = UMLFactory.eINSTANCE.createUseCase();
 			usecase.setPackage(p);
-			TransformHelper.transform(e, usecase);
+			t.transform(e, usecase);
 		}
 	}
 	
@@ -45,14 +47,16 @@ public class ElementFactory {
 	 * @param e
 	 * @param u
 	 */
-	public static void create(org.sparx.Element e, UseCase u) {
+	public static void create(org.sparx.Element e, UseCase u, TransformHelper _t) {
 		log.info("Create Element for " + e.GetName());
+
+		TransformHelper t = new TransformHelper();
 
 		if ( e.GetType().equals("Activity") ) {
 			log.info("Element is an Activity");
 			Activity activity = UMLFactory.eINSTANCE.createActivity();
 			activity = (Activity)u.createOwnedBehavior(e.GetName(), activity.eClass());
-			TransformHelper.transform(e, activity);
+			t.transform(e, activity);
 		}
 	}
 	
@@ -61,60 +65,80 @@ public class ElementFactory {
 	 * @param e
 	 * @param a
 	 */
-	public static void create(org.sparx.Element e, Activity a) {
+	public static void create(org.sparx.Element e, Activity a, TransformHelper _t) {
 		log.debug("Create Element for " + e.GetName());
 
 		if ( e.GetType().equals("Action") && e.GetClassfierID() > 0) {
 			log.info("Element is a CallBehaviourAction");
 			Action action = UMLFactory.eINSTANCE.createCallBehaviorAction();
 			action.setActivity(a);
-			TransformHelper.transform(e, action);
+			_t.transform(e, action);
 		}
 		else if ( e.GetType().equals("Action")) {
 			log.info("Element is an Action");
 			Action action = UMLFactory.eINSTANCE.createCallOperationAction();
 			action.setActivity(a);
-			TransformHelper.transform(e, action);
+			_t.transform(e, action);
 		}
 		else if (e.GetType().equals("Activity") ) {
 			log.info("Element is an Activity");
 			Activity activity = UMLFactory.eINSTANCE.createActivity();
 			activity = (Activity)a.createOwnedBehavior(e.GetName(), activity.eClass());
-			TransformHelper.transform(e, activity);
+			TransformHelper t = new TransformHelper();
+			t.transform(e, activity);
 		}
 		else if (e.GetType().equals("StateNode") && e.GetSubtype() == 100 ) {
 			log.info("Element is an ActivityInitial");
 			InitialNode initialNode = UMLFactory.eINSTANCE.createInitialNode();
 			initialNode.setActivity(a);
-			TransformHelper.transform(e, initialNode);
+			_t.transform(e, initialNode);
 		}
 		else if (e.GetType().equals("StateNode") && e.GetSubtype() == 101 ) {
 			log.info("Element is an ActivityFinial");
 			ActivityFinalNode finalNode = UMLFactory.eINSTANCE.createActivityFinalNode();
 			finalNode.setActivity(a);
-			TransformHelper.transform(e, finalNode);
+			_t.transform(e, finalNode);
 		}
 		else if (e.GetType().equals("StateNode") && e.GetSubtype() == 102 ) {
 			log.info("Element is an FlowFinal");
 			FlowFinalNode finalNode = UMLFactory.eINSTANCE.createFlowFinalNode();
 			finalNode.setActivity(a);
-			TransformHelper.transform(e, finalNode);
+			_t.transform(e, finalNode);
 		}
 
 	}
 
 	/**
+	 * Creates activity edges
+	 * @param c
+	 * @param a
+	 */
+	public static void create(org.sparx.Connector c, Activity a, TransformHelper _t) {
+		log.debug("Create connector " + c.GetConnectorGUID());
+		
+		// at the moment we create only control flows
+		ControlFlow activityEdge = UMLFactory.eINSTANCE.createControlFlow();
+		activityEdge = (ControlFlow)a.createEdge(c.GetName(), activityEdge.eClass());
+		_t.transform(c, activityEdge);
+	}
+	
+	/**
 	 * Creates children element of an action
 	 * @param e
 	 * @param a
 	 */
-	public static void create(org.sparx.Element e, Action a) {
+	public static void create(org.sparx.Element e, Action a, TransformHelper _t) {
 		log.debug("Create Element for " + e.GetName());
 		
+		TransformHelper t = new TransformHelper();
+
+		// Maybe, an action contains an activity as a child element
+		// Then, the real parent of this activity would be the actions parent activity
 		if (e.GetType().equals("Activity") ) {
 			log.info("Element is an Activity");
 			Activity activity = UMLFactory.eINSTANCE.createActivity();
-			TransformHelper.transform(e, activity);
+			activity = (Activity)a.getActivity().createOwnedBehavior(e.GetName(), activity.eClass());
+			t.transform(e, activity);
 		}
 	}
 }
--- de.genesez.adapter.ea/src/de/genesez/adapter/ea/TransformHelper.java
+++ de.genesez.adapter.ea/src/de/genesez/adapter/ea/TransformHelper.java
@@ -1,11 +1,19 @@
 package de.genesez.adapter.ea;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.emf.ecore.xmi.XMIResource;
 import org.eclipse.uml2.uml.Action;
 import org.eclipse.uml2.uml.Activity;
+import org.eclipse.uml2.uml.ActivityEdge;
+import org.eclipse.uml2.uml.ActivityNode;
 import org.eclipse.uml2.uml.Actor;
+import org.eclipse.uml2.uml.ControlFlow;
 import org.eclipse.uml2.uml.FinalNode;
 import org.eclipse.uml2.uml.InitialNode;
 import org.eclipse.uml2.uml.Model;
@@ -17,10 +25,15 @@ import org.sparx.Collection;
 
 public class TransformHelper {
 
-	private final static Log log = LogFactory.getLog(XmiWriter.class);
+	private final static Log log = LogFactory.getLog(TransformHelper.class);
 	private static XMIResource resource = null;
-	
-	public static void setResource(XMIResource r) {
+
+	// these are just helpers... maybe we'll beautify it some day
+	private Map<String, ActivityEdge> edges = new HashMap<String, ActivityEdge>();
+	private Map<Integer, Set<String>> edgeSources = new HashMap<Integer, Set<String>>();
+	private Map<Integer, Set<String>> edgeTargets = new HashMap<Integer, Set<String>>();
+
+	public void setResource(XMIResource r) {
 		TransformHelper.resource = r;
 	}
 	
@@ -29,9 +42,9 @@ public class TransformHelper {
 	 * @param in	the EA package reference of the model
 	 * @return		the UML model reference
 	 */
-	public static Model transform(org.sparx.Package in) {
+	public Model transform(org.sparx.Package in) {
 		Model m = UMLFactory.eINSTANCE.createModel();
-		TransformHelper.transform(in, m);
+		this.transform(in, m);
 		return m;
 	}
 	/**
@@ -39,7 +52,7 @@ public class TransformHelper {
 	 * @param in	the EA package reference of the model
 	 * @param out	the UML model reference
 	 */
-	public static void transform(org.sparx.Package in, Model out) {
+	public void transform(org.sparx.Package in, Model out) {
 		log.info("Transforming Model: " + in.GetName());
 		out.setName(in.GetName());
 		TransformHelper.resource.setID(out, in.GetPackageGUID());
@@ -48,7 +61,7 @@ public class TransformHelper {
 		Collection<org.sparx.Package> pkgs = in.GetPackages();
 		for (org.sparx.Package p : pkgs) {
 			Package _p = out.createNestedPackage(p.GetName());
-			TransformHelper.transform(p, _p);
+			this.transform(p, _p);
 		}
 	}
 	
@@ -57,7 +70,7 @@ public class TransformHelper {
 	 * @param in	the EA package reference
 	 * @param out	the UML2 package reference
 	 */
-	public static void transform(org.sparx.Package in, Package out) {
+	public void transform(org.sparx.Package in, Package out) {
 		log.info("Transforming Package: " + in.GetName());
 		out.setName(in.GetName());
 		TransformHelper.resource.setID(out, in.GetPackageGUID());
@@ -66,14 +79,14 @@ public class TransformHelper {
 		Collection<org.sparx.Package> pkgs = in.GetPackages();
 		for (org.sparx.Package p : pkgs) {
 			Package _p = out.createNestedPackage(p.GetName());
-			TransformHelper.transform(p, _p);
+			this.transform(p, _p);
 		}
 
 		// Transform elements in package (parentId == 0)
 		Collection<org.sparx.Element> elms = in.GetElements();
 		for (org.sparx.Element e : elms) {
 			if ( 0 == e.GetParentID() ) {
-				ElementFactory.create(e, out);
+				ElementFactory.create(e, out, this);
 			}
 		}
 	}
@@ -83,9 +96,10 @@ public class TransformHelper {
 	 * @param in	the EA actor reference
 	 * @param out	the UML2 package reference
 	 */
-	public static void transform(org.sparx.Element in, Actor out) {
+	public void transform(org.sparx.Element in, Actor out) {
 		log.info("Transforming Actor: " + in.GetName());
-		basicTransform(in, out);
+		this.basicTransform(in, out);
+		log.info("Actor transformed");
 	}
 
 	/**
@@ -93,13 +107,13 @@ public class TransformHelper {
 	 * @param in	the EA usecase reference
 	 * @param out	the UML2 usecase reference
 	 */
-	public static void transform(org.sparx.Element in, UseCase out) {
+	public void transform(org.sparx.Element in, UseCase out) {
 		log.info("Transforming UseCase: " + in.GetName());
-		basicTransform(in, out);
+		this.basicTransform(in, out);
 
 		Collection<org.sparx.Element> elms = in.GetElements();
 		for (org.sparx.Element e : elms) {
-			ElementFactory.create(e, out);
+			ElementFactory.create(e, out, this);
 		}
 	}
 	
@@ -108,13 +122,52 @@ public class TransformHelper {
 	 * @param in	the EA activity reference
 	 * @param out	the UML2 activity reference
 	 */
-	public static void transform(org.sparx.Element in, Activity out) {
+	public void transform(org.sparx.Element in, Activity out) {
 		log.info("Transforming Activity: " + in.GetName());
-		basicTransform(in, out);
+		this.basicTransform(in, out);
 
 		Collection<org.sparx.Element> elms = in.GetElements();
 		for (org.sparx.Element e : elms) {
-			ElementFactory.create(e, out);
+			for (org.sparx.Connector c : e.GetConnectors()) {
+				// Connectors are always found two times:
+				// At the source node and at the target node
+				if (this.edges.containsKey(c.GetConnectorGUID())) {
+					continue;
+				}
+				
+				ElementFactory.create(c, out, this);
+			}
+		}
+
+		for (org.sparx.Element e : elms) {
+			ElementFactory.create(e, out, this);
+		}
+	}
+
+	/**
+	 * 
+	 */
+	public void transform(org.sparx.Connector in, ControlFlow out) {
+		log.info("Transforming ControlFlow: " + in.GetName());
+		this.basicTransform(in, out);
+
+		// remember the connector guid and the corresponding ActivityEdge
+		this.edges.put(in.GetConnectorGUID(), out);
+
+		// create a dummy HashSet for the connectors guid 
+		HashSet<String> connectorGuid = new HashSet<String>();
+		connectorGuid.add(in.GetConnectorGUID());
+		// add the connector to its sources connector list
+		if (this.edgeSources.containsKey(in.GetClientID())) {
+			this.edgeSources.get(in.GetClientID()).addAll(connectorGuid);
+		} else {
+			this.edgeSources.put(in.GetClientID(), connectorGuid);
+		}
+		// add the connector to its targets connector list
+		if (this.edgeTargets.containsKey(in.GetSupplierID())) {
+			this.edgeTargets.get(in.GetSupplierID()).addAll(connectorGuid);
+		} else {
+			this.edgeTargets.put(in.GetSupplierID(), connectorGuid);
 		}
 	}
 	
@@ -123,13 +176,14 @@ public class TransformHelper {
 	 * @param in	the EA action reference
 	 * @param out	the UML2 action reference
 	 */
-	public static void transform(org.sparx.Element in, Action out) {
+	public void transform(org.sparx.Element in, Action out) {
 		log.info("Transforming Action: " + in.GetName());
-		basicTransform(in, out);
+		this.basicTransform(in, out);
+		this.applyEdges(in, out);
 
 		Collection<org.sparx.Element> elms = in.GetElements();
 		for (org.sparx.Element e : elms) {
-			ElementFactory.create(e, out);
+			ElementFactory.create(e, out, this);
 		}
 	}
 
@@ -138,9 +192,10 @@ public class TransformHelper {
 	 * @param in	the EA initialnode reference
 	 * @param out	the UML2 initialnode reference
 	 */
-	public static void transform(org.sparx.Element in, InitialNode out) {
+	public void transform(org.sparx.Element in, InitialNode out) {
 		log.info("Transforming IntitialNode: " + in.GetName());
-		basicTransform(in, out);
+		this.basicTransform(in, out);
+		this.applyEdges(in, out);
 	}
 
 	/**
@@ -148,9 +203,10 @@ public class TransformHelper {
 	 * @param in	the EA finalnode reference
 	 * @param out	the UML2 finalnode reference
 	 */
-	public static void transform(org.sparx.Element in, FinalNode out) {
+	public void transform(org.sparx.Element in, FinalNode out) {
 		log.info("Transforming FinalNode: " + in.GetName());
-		basicTransform(in, out);
+		this.basicTransform(in, out);
+		this.applyEdges(in, out);
 	}
 	
 	/**
@@ -158,10 +214,44 @@ public class TransformHelper {
 	 * @param in	the EA element reference
 	 * @param out	the UML named element reference 
 	 */
-	private static void basicTransform(org.sparx.Element in, NamedElement out) {
+	private void basicTransform(org.sparx.Element in, NamedElement out) {
+		log.debug("Starting basic transformation for Element");
 		out.setName(in.GetName());
 		TransformHelper.resource.setID(out, in.GetElementGUID());
-		TransformHelper.applyStereotypes(in, out);
+		this.applyStereotypes(in, out);
+	}
+
+	/**
+	 * Basic transformations for all connectors
+	 * @param in	the EA connector reference
+	 * @param out	the UML named element reference 
+	 */
+	private void basicTransform(org.sparx.Connector in, NamedElement out) {
+		log.debug("Starting basic transformation for Connector");
+		out.setName(in.GetName());
+		TransformHelper.resource.setID(out, in.GetConnectorGUID());
+	}
+
+	/**
+	 * Applies the source and target activity nodes to the activity edges 
+	 * @param in
+	 * @param out
+	 */
+	private void applyEdges(org.sparx.Element in, ActivityNode out) {
+		
+		if (this.edgeSources.containsKey(in.GetElementID())) {
+			log.debug("Outgoing Flows:" + this.edgeSources.get(in.GetElementID()).size());
+			for (String connectorGuid: this.edgeSources.get(in.GetElementID()) ) {
+				this.edges.get(connectorGuid).setSource(out);
+			}
+		}
+		
+		if (this.edgeTargets.containsKey(in.GetElementID())) {
+			log.debug("Incoming Flows:" + this.edgeTargets.get(in.GetElementID()).size());
+			for (String connectorGuid: this.edgeTargets.get(in.GetElementID()) ) {
+				this.edges.get(connectorGuid).setTarget(out);
+			}
+		}
 	}
 	
 	/**
@@ -169,18 +259,18 @@ public class TransformHelper {
 	 * @param in	the EA element reference
 	 * @param out	the UML named element reference
 	 */
-	private static void applyStereotypes(org.sparx.Element in, NamedElement out) {
-		log.info("Transforming FinalNode: " + in.GetName());
+	private void applyStereotypes(org.sparx.Element in, NamedElement out) {
+		log.info("Applying Stereotypes: " + in.GetName());
 		
-		// Read and apply stereotypes
-		for (String s : in.GetStereotypeList().split(",")) {
-			log.info("Apply stereotype " + s);
-		}
-		
-		// Set tagged values
-		Collection<org.sparx.TaggedValue> tgvs = in.GetTaggedValues();
-		for (org.sparx.TaggedValue t : tgvs) {
-			log.info("Set tagged value" + t.GetName() + " to " + t.GetValue());
-		}
+//		// Read and apply stereotypes
+//		for (String s : in.GetStereotypeList().split(",")) {
+//			log.info("Apply stereotype " + s);
+//		}
+//		
+//		// Set tagged values
+//		Collection<org.sparx.TaggedValue> tgvs = in.GetTaggedValues();
+//		for (org.sparx.TaggedValue t : tgvs) {
+//			log.info("Set tagged value" + t.GetName() + " to " + t.GetValue());
+//		}
 	}
 }
