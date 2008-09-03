c11f5db854211b9ccdcc5788b836ec12a65a47bb
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2008-09-03 19:55:21 +0000

    added mindmap for student task

---
+++ de.genesez.docs/wiki/tasks/2008-06-21_Statistik-Komponente.mm
@@ -0,0 +1,28 @@
+<map version="0.9.0_Beta_8">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1214045970968" ID="Freemind_Link_1218739944" MODIFIED="1214045978859" TEXT="Statistik Komponente">
+<node CREATED="1214045980875" ID="Freemind_Link_870966966" MODIFIED="1214046029640" POSITION="right" TEXT="Berechnung Code-Verh&#xe4;ltnis&#xa;generiert &lt;-&gt; nicht generiert"/>
+<node CREATED="1214046034968" ID="Freemind_Link_1653781727" MODIFIED="1214046049671" POSITION="left" TEXT="Gliederung">
+<node CREATED="1214046054000" ID="Freemind_Link_1727127112" MODIFIED="1214046055515" TEXT="File"/>
+<node CREATED="1214046057437" ID="Freemind_Link_1590877188" MODIFIED="1214046064921" TEXT="Class / Type"/>
+<node CREATED="1214046066531" ID="Freemind_Link_636156062" MODIFIED="1214046071437" TEXT="Package / Namespace"/>
+<node CREATED="1214046073328" ID="Freemind_Link_796606223" MODIFIED="1214046082718" TEXT="Model / Projekt"/>
+</node>
+<node CREATED="1214046232187" ID="Freemind_Link_1114551703" MODIFIED="1214046235781" POSITION="right" TEXT="Core">
+<node CREATED="1214046239515" ID="Freemind_Link_1784738657" MODIFIED="1214046240796" TEXT="Java"/>
+</node>
+<node CREATED="1214046102750" ID="Freemind_Link_1851198477" MODIFIED="1214046110421" POSITION="right" TEXT="Frontend">
+<node CREATED="1214046112781" ID="Freemind_Link_1388658419" MODIFIED="1214046156984" TEXT="openArchitectureWare Workflow Komponente"/>
+<node CREATED="1214046159390" ID="Freemind_Link_555489974" MODIFIED="1214046162875" TEXT="ANT Task"/>
+</node>
+<node CREATED="1214046251078" ID="Freemind_Link_829521878" MODIFIED="1214046259750" POSITION="right" TEXT="View">
+<node CREATED="1214046261875" ID="Freemind_Link_1399783648" MODIFIED="1214046263203" TEXT="XML"/>
+<node CREATED="1214046265359" ID="Freemind_Link_949013223" MODIFIED="1214046274750" TEXT="XSLT (XML -&gt; HTML)"/>
+<node CREATED="1214046337593" ID="Freemind_Link_1371130238" MODIFIED="1214046361437" TEXT="( Integration in Projekt-Doku / Projekt-Homepage / Test-Reports )"/>
+</node>
+<node CREATED="1214046202625" ID="Freemind_Link_1895989920" MODIFIED="1214046455421" POSITION="left" TEXT="Basis">
+<node CREATED="1214046459437" ID="Freemind_Link_1538086262" MODIFIED="1214046464484" TEXT="Protected Regions"/>
+<node CREATED="1214046466562" ID="Freemind_Link_1675939372" MODIFIED="1214046467968" TEXT="Lines of Code?"/>
+</node>
+</node>
+</map>
