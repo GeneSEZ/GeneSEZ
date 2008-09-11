0f6599f3fb960a901af4b0062e74b7980edd5f7a
olar <olar@a19731f8-c322-0410-8b9b-f98442d78366>
2008-09-11 17:14:37 +0000

    First check-in for forms example in csharp

---
+++ trunk/de.genesez.example.csharp.forms/src-gen/forms/Line.cs
@@ -0,0 +1,42 @@
+namespace forms
+{
+	#region Imports
+	using System;
+	/* PROTECTED REGION ID(java.mclassifier.other.import.code_14_0_b6f02e1_1211871374078_396988_308) ENABLED START */
+	/* TODO put your imports here */
+
+	/* PROTECTED REGION END */
+	#endregion
+
+	public class Line : IForm
+	{
+		#region Private Fields
+<< << <<<.working
+		private  =======
+		        private   >> >> >>>.merge-right.r1466
+		        Point start;
+<< << <<<.working
+		private  =======
+		        private   >> >> >>>.merge-right.r1466
+		        Point end;
+		#endregion
+
+		#region Constructors
+		public Line( ref Point start, ref Point end )
+		{
+			/* PROTECTED REGION ID(java.moperation.implementation.operation.code._14_0_b6f02e1_1211871400078_573707_331) ENABLED START */
+			/* TODO put your own implementation code here */
+
+			/* PROTECTED REGION END */
+		}
+
+		#endregion
+
+		#region Your Own Implementations
+		/* PROTECTED REGION ID(java.mclassifier.implementation.owncode.code._14_0_b6f02e1_1211871374078_396988_308) ENABLED START */
+		/* TODO put your own implementation code here */
+
+		/* PROTECTED REGION END */
+		#endregion
+	}
+}
---
+++ trunk/de.genesez.example.csharp.forms/src-gen/forms/Point.cs
@@ -0,0 +1,42 @@
+namespace forms
+{
+	#region Imports
+	using System;
+	/* PROTECTED REGION ID(java.mclassifier.other.import.code_14_0_b6f02e1_1211871095796_723781_254) ENABLED START */
+	/* TODO put your imports here */
+
+	/* PROTECTED REGION END */
+	#endregion
+
+	public class Point : IForm
+	{
+		#region Private Fields
+<< << <<<.working
+		private  =======
+		        private   >> >> >>>.merge-right.r1466
+		        double x;
+<< << <<<.working
+		private  =======
+		        private   >> >> >>>.merge-right.r1466
+		        double y;
+		#endregion
+
+		#region Constructors
+		public Point()
+		{
+			/* PROTECTED REGION ID(java.moperation.implementation.operation.code._14_0_b6f02e1_1211871240109_473577_277) ENABLED START */
+			/* TODO put your own implementation code here */
+
+			/* PROTECTED REGION END */
+		}
+
+		#endregion
+
+		#region Your Own Implementations
+		/* PROTECTED REGION ID(java.mclassifier.implementation.owncode.code._14_0_b6f02e1_1211871095796_723781_254) ENABLED START */
+		/* TODO put your own implementation code here */
+
+		/* PROTECTED REGION END */
+		#endregion
+	}
+}
