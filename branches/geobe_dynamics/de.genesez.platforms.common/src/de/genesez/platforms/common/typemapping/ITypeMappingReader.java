17015729c33ab02cdba985e4876f90c0bd9fe7e8
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2008-10-22 12:04:15 +0000

    branched for state machine support

---
+++ branches/geobe_dynamics/de.genesez.platforms.common/src/de/genesez/platforms/common/typemapping/ITypeMappingReader.java
@@ -0,0 +1,11 @@
+package de.genesez.platforms.common.typemapping;
+
+/**
+ * Basic interface for all type mapping readers.
+ * 
+ * @author nihe
+ * @date 2008-02-18
+ */
+public interface ITypeMappingReader {
+
+}
