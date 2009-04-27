8c71612eff2c5202c04e3fb0722e9c121fd25bf4
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2009-04-27 09:38:17 +0000

    initial commit of ddm + utility module

---
+++ projects/de.genesez.platforms.php.metaframework.modules/trunk/de.genesez.platforms.php.metaframework.modules/de.genesez.modules.util/Msg/Message.php
@@ -0,0 +1,42 @@
+<?php
+require_once 'Core/BaseDto.php';
+
+/* PROTECTED REGION ID(php.own.imports._16_0_b6f02e1_1240322753437_323386_425) ENABLED START */
+// TODO: put your further include + require statements here
+/* PROTECTED REGION END */
+
+/**
+ * Simple class to represent a message.
+ * @see		Core_BaseDto
+ * @author	dreamer
+ * @package	Utilities
+ */
+class Msg_Message extends Core_BaseDto  {
+
+	// -- constructors + destructors ----------------------------------------
+	
+	/**
+	 * constructs an object of class {@link Msg_Message}
+	 * @generated	constructor stub for implementation
+	 * @param	string	$message	
+	 */
+	public function __construct($message) {
+		/* PROTECTED REGION ID(php.constructor._16_0_b6f02e1_1240322762234_410753_447) ENABLED START */
+		parent::__construct('utilities/message.html');
+		$this->message = $message;
+		/* PROTECTED REGION END */
+	}
+
+
+
+	// -- method implementations --------------------------------------------
+
+
+
+
+	// -- own code implementation -------------------------------------------
+	/* PROTECTED REGION ID(php.class.own.code.implementation._16_0_b6f02e1_1240322753437_323386_425) ENABLED START */
+	// TODO: put your further code implementations for class 'Msg_Message' here
+	/* PROTECTED REGION END */
+}
+?>
---
+++ projects/de.genesez.platforms.php.metaframework.modules/trunk/de.genesez.platforms.php.metaframework.modules/de.genesez.modules.util/UtilitiesModuleLoader.php
@@ -0,0 +1,40 @@
+<?php
+require_once 'Loader/InvisibleModuleLoader.php';
+
+/* PROTECTED REGION ID(php.own.imports._16_0_b6f02e1_1240324483359_514108_877) ENABLED START */
+// TODO: put your further include + require statements here
+/* PROTECTED REGION END */
+
+/**
+ * @see		Loader_InvisibleModuleLoader
+ * @author	dreamer
+ * @package	Utilities
+ */
+class UtilitiesModuleLoader extends Loader_InvisibleModuleLoader  {
+
+
+
+
+	// -- method implementations --------------------------------------------
+	
+	/**
+	 * @generated	method stub for implementation
+	 * @return	array of S2Container_ComponentDef
+	 */
+	public function getComponents() {
+		/* PROTECTED REGION ID(php.implementation._16_0_b6f02e1_1240324545343_620417_920) ENABLED START */
+		return array(
+			Adapter_SeasarPhpBuilder::newComponent('FlashNotifier', 'notifier')
+		);
+		/* PROTECTED REGION END */
+	}
+
+
+
+
+	// -- own code implementation -------------------------------------------
+	/* PROTECTED REGION ID(php.class.own.code.implementation._16_0_b6f02e1_1240324483359_514108_877) ENABLED START */
+	// TODO: put your further code implementations for class 'UtilitiesModuleLoader' here
+	/* PROTECTED REGION END */
+}
+?>
