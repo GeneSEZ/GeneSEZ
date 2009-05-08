380547391509cccb9d73d4b6315d28352b143678
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2009-05-08 21:42:46 +0000

    revised the template function, added comments, added url fallback to determine default templates

--- projects/de.genesez.platforms.php.metaframework/src/Adapter/SmartyRenderer.php
+++ projects/de.genesez.platforms.php.metaframework/src/Adapter/SmartyRenderer.php
@@ -21,11 +21,6 @@ class Adapter_SmartyRenderer  implements Core_Renderer {
 	 * @var		Smarty	$smarty
 	 */
 	protected $smarty;
-	/**
-	 * @generated	attribute definition
-	 * @var		string	$templateBasePath
-	 */
-	protected $templateBasePath;
 
 
 
@@ -48,50 +43,112 @@ class Adapter_SmartyRenderer  implements Core_Renderer {
 	}
 
 	/**
-	 * tries to find a template resource for a specified, non-existing template 
-	 * resource. This method is registered as 
-	 * &lt;code&gt;default_template_handler_func&lt;/code&gt;. Smarty uses this method to 
-	 * obtain the source of a template when the specified template resource 
-	 * doesn't exist.
-	 * @param	string	$resource_type	the type of the non-existing template resource
-	 * @param	string	$resource_name	the name of the non-existing template resource
-	 * @param	string	$template_source	variable to return the template content
-	 * @param	int	$template_timestamp	variable to return the timestamp of the template content
-	 * @param	Smarty	$smarty_obj	reference to the smarty object
-	 * @return	boolean
+	 * The dynamically registered template function plugin <b>render</b> for 
+	 * smarty templates. It is the smarty template function port of the <b>Renderer</b> 
+	 * interface, i.e. you specifiy only a dto to render it. If there is no 
+	 * &lt;code&gt;dto&lt;/code&gt; parameter with an instance of type &lt;code&gt;Core_Dto&lt;/code&gt; 
+	 * a smarty error is triggered.<br><br>The view of a dto must not exist; in 
+	 * this case this function derives a default template from the URL.<br>
+	 * @param	array	$params	the parameters of the template function as an associative array with key:value pairs
+	 * @param	Smarty	$smarty	reference to the smarty object
+	 * @return	string
 	 */
-	public function defaultTemplateHandler($resource_type, $resource_name, &$template_source, &$template_timestamp, &$smarty_obj) {
-		/* PROTECTED REGION ID(php.implementation._16_0_b6f02e1_1241024432671_172546_369) ENABLED START */
-		// this does NOT solve the problems!  unable to change the resource name :-(
-		// filling template_source results in wrong content of compiled templates
-		// calling smarty->display does NOT avoid the trigger_error, template "" not found
-		if ($resource_type == 'file') {
-			// check default suffixes for templates
-			foreach ($this->defaultSuffixes() as $value) {
-				$view = $this->buildView($value);
+	public function checkInclude($params, &$smarty) {
+		/* PROTECTED REGION ID(php.implementation._16_0_b6f02e1_1241804745078_804374_559) ENABLED START */
+		if (array_key_exists('dto', $params)) {
+			if ($params['dto'] instanceof Core_Dto) {
+				$view = $params['dto']->view();
 				if ($this->isSmartyTemplate($view)) {
-					// does not work: caching problems, every template determined by this function has same compiled template name
-//					$template_source = file_get_contents($base . $view);
-//					$template_timestamp = filemtime($base . $view);
-					$smarty_obj->display($view);
-					return true;
+					return $this->renderInclude($view, $params);
+				}
+				return $this->processInclude($view, $params);
+			} else {
+				$this->smarty->trigger_error('unable to render include: the dto type (' . gettype($params['dto']) . ') must implement the \'Core_Dto\' interface! ');
+			}
+		} else {
+			$this->smarty->trigger_error('unable to render include: there is no \'dto\' parameter! ' . print_r($params, true));
+		}
+		/* PROTECTED REGION END */
+	}
+
+	/**
+	 * Tries to determine a default smarty template to render the template 
+	 * function parameters, that is the data transfer object.
+	 * <br>
+	 * <br>If successful, the smarty template found is rendered, otherwise a smarty 
+	 * error is triggered.
+	 * @param	string	$view	the template as specified by the data transfer object
+	 * @param	array	$params	the parameters of the template function as an associative array with key:value pairs
+	 * @return	string
+	 */
+	protected function processInclude($view, $params) {
+		/* PROTECTED REGION ID(php.implementation._16_0_b6f02e1_1241808386312_403388_581) ENABLED START */
+		if ($view === null || $view === '') {
+			// check suffixes with url fallback
+			$requestPath = Core_Url::requestInfo();
+			for ($i = count($requestPath); $i > 0; --$i) {
+				$part = array_slice($requestPath, 0, $i);
+				$view = implode('/', $part);
+				foreach ($this->defaultSuffixes() as $suffix) {
+					$name = $this->buildView($suffix, $view);
+					if ($this->isSmartyTemplate($name)) {
+						return $this->renderInclude($name, $params);
+					}
 				}
 			}
-			// check url fallback
-//			$requestPath = Core_Url::requestInfo();
-//			for ($i = count($requestPath); $i > 0; --$i) {
-//				$part = array_slice($requestPath, 0, $i);
-//				$view = implode('/', $part);
-//				foreach ($this->defaultSuffixes() as $suffix) {
-//					$name = $this->buildView($suffix, $view);
-//					if ($this->isSmartyTemplate($name)) {
-//						$smarty_obj->display($view);
-//						return true;
-//					}
-//				}
-//			}
+		} else {
+			// check only suffixes
+			foreach ($this->defaultSuffixes() as $suffix) {
+				$name = $this->buildView($suffix, $view);
+				if ($this->isSmartyTemplate($name)) {
+					return $this->renderInclude($name, $params);
+				}
+			}
 		}
-		return false;
+		$this->smarty->trigger_error('unable to determine template resource: \''. $view .'\'! ' . print_r($params, true));
+		/* PROTECTED REGION END */
+	}
+
+	/**
+	 * Evaluates the specified smarty template and returns its contents, i.e. the 
+	 * content of the compiled smarty template.
+	 * @param	string	$view	a valid smarty template
+	 * @param	array	$params	the parameters of the template function as an associative array with key:value pairs
+	 * @return	string
+	 */
+	protected function renderInclude($view, $params) {
+		/* PROTECTED REGION ID(php.implementation._16_0_b6f02e1_1241807872578_14321_574) ENABLED START */
+		// TODO: exists another way to do this?
+		$smarty = new Smarty();
+		$smarty->template_dir = $this->smarty->template_dir;
+		$smarty->compile_dir = $this->smarty->compile_dir;
+		$smarty->register_function('render', array($this, 'checkInclude'));
+		$smarty->assign('webbase', Core_Url::baseServerUri());
+		$smarty->assign('requestbase', Core_Url::baseRequestUri());
+		$smarty->assign($params);
+		return $smarty->fetch($view);
+		// this does NOT work!  it works for the first time 'render' is used, further usage results in lost dto parameters
+//		return $this->smarty->_smarty_include(array(
+//			'smarty_include_tpl_file' => $view,
+//			'smarty_include_vars' => $params
+//		));
+//		return $view;
+		/* PROTECTED REGION END */
+	}
+
+	/**
+	 * Checks if the specified template is a smarty template file and readable.
+	 * @param	string	$name	the smarty template to check
+	 * @return	boolean
+	 */
+	protected function isSmartyTemplate($name) {
+		/* PROTECTED REGION ID(php.implementation._16_0_b6f02e1_1241769056828_26378_516) ENABLED START */
+		$view = realpath('./' . $this->smarty->template_dir) . '/' . $name;
+		if (is_file($view) && is_readable($view)) {
+			return true;
+		} else {
+			return false;
+		}
 		/* PROTECTED REGION END */
 	}
 
@@ -123,103 +180,6 @@ class Adapter_SmartyRenderer  implements Core_Renderer {
 	protected function defaultSuffixes() {
 		/* PROTECTED REGION ID(php.implementation._16_0_b6f02e1_1241024375453_409016_363) ENABLED START */
 		return array('', '.html', '.tpl', '/index.html', '/index.tpl');
-		/* PROTECTED REGION END */
-	}
-
-	/**
-	 * @generated	method stub for implementation
-	 * @param	string	$name	
-	 * @return	boolean
-	 */
-	protected function isSmartyTemplate($name) {
-		/* PROTECTED REGION ID(php.implementation._16_0_b6f02e1_1241769056828_26378_516) ENABLED START */
-		$view = realpath('./' . $this->smarty->template_dir) . '/' . $name;
-		if (is_file($view) && is_readable($view)) {
-			return true;
-		} else {
-			return false;
-		}
-		/* PROTECTED REGION END */
-	}
-
-	/**
-	 * @generated	method stub for implementation
-	 * @param	array	$params	array of type 'string'
-	 * @param	Smarty	$smarty	
-	 * @return	string
-	 */
-	public function checkInclude($params, &$smarty) {
-		/* PROTECTED REGION ID(php.implementation._16_0_b6f02e1_1241804745078_804374_559) ENABLED START */
-		if (array_key_exists('dto', $params)) {
-			if ($params['dto'] instanceof Core_Dto) {
-				$view = $params['dto']->view();
-				if ($this->isSmartyTemplate($view)) {
-					return $this->renderInclude($view, $params);
-				}
-				return $this->processInclude($view, $params);
-			} else {
-				$this->smarty->trigger_error('unable to render include: the dto type (' . gettype($params['dto']) . ') must implement the \'Core_Dto\' interface! ');
-			}
-		} else {
-			$this->smarty->trigger_error('unable to render include: there is no \'dto\' parameter! ' . print_r($params, true));
-		}
-		/* PROTECTED REGION END */
-	}
-
-	/**
-	 * @generated	method stub for implementation
-	 * @param	string	$view	
-	 * @param	array	$params	array of type 'string'
-	 * @return	string
-	 */
-	protected function renderInclude($view, $params) {
-		/* PROTECTED REGION ID(php.implementation._16_0_b6f02e1_1241807872578_14321_574) ENABLED START */
-		// TODO: replace this workaround!!!  but i have no idea how :-(
-		$smarty = new Smarty();
-		$smarty->template_dir = $this->smarty->template_dir;
-		$smarty->compile_dir = $this->smarty->compile_dir;
-		$smarty->register_function('render', array($this, 'checkInclude'));
-		$smarty->assign('webbase', Core_Url::baseServerUri());
-		$smarty->assign('requestbase', Core_Url::baseRequestUri());
-		$smarty->assign($params);
-		return $smarty->fetch($view);
-		// this does NOT work!  it works for the first time 'render' is used, further usage results in lost dto parameters
-//		return $this->smarty->_smarty_include(array(
-//			'smarty_include_tpl_file' => $view,
-//			'smarty_include_vars' => $params
-//		));
-//		return $view;
-		/* PROTECTED REGION END */
-	}
-
-	/**
-	 * @generated	method stub for implementation
-	 * @param	string	$view	
-	 * @param	array	$params	array of type 'string'
-	 * @return	string
-	 */
-	protected function processInclude($view, $params) {
-		/* PROTECTED REGION ID(php.implementation._16_0_b6f02e1_1241808386312_403388_581) ENABLED START */
-		if ($view === '') {
-			$view = Core_Url::requestPath();
-			// check suffixes
-			foreach ($this->defaultSuffixes() as $suffix) {
-				$name = $this->buildView($suffix, $view);
-				if ($this->isSmartyTemplate($name)) {
-					return $this->renderInclude($name, $params);
-				}
-			}
-			// url fallback
-			
-		} else {
-			foreach ($this->defaultSuffixes() as $suffix) {
-				$name = $this->buildView($suffix, $view);
-				if ($this->isSmartyTemplate($name)) {
-					return $this->renderInclude($name, $params);
-				}
-			}
-		}
-		$this->smarty->trigger_error('unable to determine template resource: \''. $view .'\'! ' . print_r($params, true));
 		/* PROTECTED REGION END */
 	}
 
