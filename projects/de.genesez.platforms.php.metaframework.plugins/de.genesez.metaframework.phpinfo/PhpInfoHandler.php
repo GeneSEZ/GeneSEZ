bf20abe8a1d3e2e72b0688ebecfe6283eb9e6092
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2009-06-22 13:58:51 +0000

    added phpinfo plug-in

---
+++ projects/de.genesez.platforms.php.metaframework.plugins/de.genesez.metaframework.phpinfo/PhpInfoHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+/* PROTECTED REGION ID(php.own.imports._16_0_b6f02e1_1245605920593_483120_768) ENABLED START */
+// TODO: put your further include + require statements here
+/* PROTECTED REGION END */
+
+/**
+ * @see		Mfw_RequestHandlerBase
+ * @author	dreamer
+ * @package	de.genesez.metaframework.phpinfo
+ */
+class PhpInfoHandler extends Mfw_RequestHandlerBase  {
+	
+	
+	
+	
+	// -- method implementations --------------------------------------------
+	
+	/**
+	 * @generated	method stub for implementation
+	 * @param	Mfw_HandlerInfo	$handlerInfo	
+	 * @return	Mfw_Dto
+	 */
+	public function handle($handlerInfo) {
+		/* PROTECTED REGION ID(php.implementation._16_0_b6f02e1_1245605931015_826023_790) ENABLED START */
+		phpinfo();
+		return true;
+		/* PROTECTED REGION END */
+	}
+	
+
+	
+	
+	
+	// -- own code implementation -------------------------------------------
+	/* PROTECTED REGION ID(php.class.own.code.implementation._16_0_b6f02e1_1245605920593_483120_768) ENABLED START */
+	// TODO: put your further code implementations for class 'PhpInfoHandler' here
+	/* PROTECTED REGION END */
+}
+?>
---
+++ projects/de.genesez.platforms.php.metaframework.plugins/de.genesez.metaframework.phpinfo/PhpInfoPlugIn.php
@@ -0,0 +1,58 @@
+<?php
+
+/* PROTECTED REGION ID(php.own.imports._16_0_b6f02e1_1245605549734_18637_652) ENABLED START */
+// TODO: put your further include + require statements here
+/* PROTECTED REGION END */
+
+/**
+ * @see		Mfw_PlugInBase
+ * @author	dreamer
+ * @package	de.genesez.metaframework.phpinfo
+ */
+class PhpInfoPlugIn extends Mfw_PlugInBase  {
+	
+	
+	
+	
+	// -- method implementations --------------------------------------------
+	
+	/**
+	 * @generated	method stub for implementation
+	 * @return	string
+	 */
+	public function getId() {
+		return 'de.genesez.metaframework.phpinfo';
+	}
+
+	/**
+	 * @generated	method stub for implementation
+	 * @return	Mfw_Context
+	 */
+	public function getContext() {
+		/* PROTECTED REGION ID(php.implementation._16_0_b6f02e1_1245605581609_541946_674) ENABLED START */
+		return new Core_Context('phpinfo', 'phpinfo.handler');
+		/* PROTECTED REGION END */
+	}
+
+	/**
+	 * @generated	method stub for implementation
+	 * @return	array of mixed
+	 */
+	public function getComponents() {
+		/* PROTECTED REGION ID(php.implementation._16_0_b6f02e1_1245605602328_933381_683) ENABLED START */
+		return array(
+			Adapter_SeasarPhpBuilder::newComponent('PhpInfoHandler', 'phpinfo.handler')
+		);
+		/* PROTECTED REGION END */
+	}
+	
+
+	
+	
+	
+	// -- own code implementation -------------------------------------------
+	/* PROTECTED REGION ID(php.class.own.code.implementation._16_0_b6f02e1_1245605549734_18637_652) ENABLED START */
+	// TODO: put your further code implementations for class 'PhpInfoPlugIn' here
+	/* PROTECTED REGION END */
+}
+?>
