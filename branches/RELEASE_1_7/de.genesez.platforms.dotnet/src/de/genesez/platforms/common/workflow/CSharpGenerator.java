4ffdc11794636c591285dffdafdd3659fca42525
gerbe <gerbe@a19731f8-c322-0410-8b9b-f98442d78366>
2009-11-28 16:48:01 +0000

    Merged from branch 1.6 up to revision 1899

---
+++ branches/RELEASE_1_7/de.genesez.platforms.dotnet/src/de/genesez/platforms/common/workflow/CSharpGenerator.java
@@ -0,0 +1,66 @@
+package de.genesez.platforms.common.workflow;
+
+import java.util.Properties;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openarchitectureware.workflow.issues.Issues;
+
+/**
+ * Performs model to text transformations for the .net platform and C#.
+ * @author Aibek Isaev
+ */
+public class CSharpGenerator extends Generator {
+	
+	private static final String propertyFile = CSharpGenerator.class.getSimpleName() + ".properties";
+	private static final Properties defaults = new Properties();
+	static {
+		defaults.put("template", "de::genesez::platforms::dotnet::csharp::templates::Root::Root");
+		defaults.put("typeMappingFile", "de/genesez/platforms/dotnet/csharp/typemapping/typemapping.xml");
+	}
+	
+	private Log logger = LogFactory.getLog(getClass());
+	private boolean isNotSetTemplate = true;
+	private boolean isNotAddTypeMappingFile = true;
+	
+	
+	public CSharpGenerator() {
+		super();
+		properties.putAll(defaults);
+		WorkflowUtils.loadAllProperties(properties, logger, propertyFile);
+	}
+	
+	/**
+	 * Overridden for checking all parameters. If parameter's value is null,
+	 * then set default value.
+	 * 
+	 * @see Generator#checkConfigurationInternal(org.openarchitectureware.workflow.issues.Issues)
+	 */
+	@Override
+	protected void checkConfigurationInternal(Issues issues) {
+		if (isNotSetTemplate) super.setTemplate(properties.getProperty("template"));
+		if (isNotAddTypeMappingFile) super.addTypeMappingFile(properties.getProperty("typeMappingFile"));
+		super.checkConfigurationInternal(issues);
+	}
+	
+	/**
+	 * setter for Template parameter in super class
+	 * 
+	 * @see Generator#setTemplate(java.lang.String)
+	 */
+	public void setTemplate(String template) {
+		super.setTemplate(template);
+		isNotSetTemplate = false;
+	}
+	
+	/**
+	 * setter for the type mapping file
+	 * 
+	 * @param typeMappingFile
+	 *            name of the file used by the generator for type mapping
+	 */
+	public void addTypeMappingFile(String typeMappingFile) {
+		super.addTypeMappingFile(typeMappingFile);
+		isNotAddTypeMappingFile = false;
+	}
+}
