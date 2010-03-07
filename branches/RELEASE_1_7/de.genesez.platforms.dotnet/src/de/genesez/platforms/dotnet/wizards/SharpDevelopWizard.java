fc2e2ad01dbd98a3811c8fcc9751b5a8f37cc2a1
gerbe <gerbe@a19731f8-c322-0410-8b9b-f98442d78366>
2010-03-07 14:43:22 +0000

    Use newly created DSL based project wizard

--- branches/RELEASE_1_7/de.genesez.platforms.dotnet/src/de/genesez/platforms/dotnet/wizards/SharpDevelopWizard.java
+++ branches/RELEASE_1_7/de.genesez.platforms.dotnet/src/de/genesez/platforms/dotnet/wizards/SharpDevelopWizard.java
@@ -1,9 +1,5 @@
 package de.genesez.platforms.dotnet.wizards;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 import de.genesez.core.IPlatformWizard;
 
 public class SharpDevelopWizard implements IPlatformWizard {
@@ -12,30 +8,7 @@ public class SharpDevelopWizard implements IPlatformWizard {
 		return "GeneSEZ .NET Platform for SharpDevelop";
 	}
 
-	public Set<String> getBundles() {
-		Set<String> bundles = new HashSet<String>();
-		bundles.add("de.genesez.platforms.dotnet;visibility:=reexport");
-		return bundles;
+	public String getModel() {
+		return "de/genesez/platforms/dotnet/wizards/sharpdevelop.project";
 	}
-
-	public Set<String> getGeneratorDirectories() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Map<String, String> getGeneratorFiles() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Set<String> getProjectDirectories() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public Map<String, String> getProjectFiles() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
 }
