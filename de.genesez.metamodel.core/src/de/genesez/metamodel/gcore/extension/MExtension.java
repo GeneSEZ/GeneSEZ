9948447855e57c7546a43e06a02b04d6b326a5f2
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2010-12-18 04:54:43 +0000

    added classes for easier java based model extensions + a class for genesez tracability extensions

---
+++ de.genesez.metamodel.core/src/de/genesez/metamodel/gcore/extension/MExtension.java
@@ -0,0 +1,23 @@
+package de.genesez.metamodel.gcore.extension;
+
+import de.genesez.metamodel.gcore.MElement;
+import de.genesez.metamodel.gcore.MModel;
+
+public interface MExtension<S extends MExtension.Stereotype, T extends MExtension.Tag> {
+	
+	public interface Stereotype {
+		String getName();
+		String getXmiGuid();
+	}
+	
+	public interface Tag {
+		String getName();
+		String getType();
+	}
+	
+	public void init(MModel model);
+	public boolean hasStereotype(MElement element, S stereotype);
+	public boolean hasTaggedValue(MElement element, S stereotype, T tag, String value);
+	public void assign(MElement element, S stereotype);
+	public void assign(MElement element, S stereotype, T tag, String value);
+}
