565646f91cf58b2cfa1173b39d9844f269a54bdd
nihe <nihe@a19731f8-c322-0410-8b9b-f98442d78366>
2011-07-15 07:48:05 +0000

    Add new Java method to set default properties through Java reflection mechanism. At the moment it is only use in TYPO3 platform.

--- trunk/de.genesez.platforms.common/src/de/genesez/platforms/common/workflow/WorkflowUtils.java
+++ trunk/de.genesez.platforms.common/src/de/genesez/platforms/common/workflow/WorkflowUtils.java
@@ -1,5 +1,6 @@
 package de.genesez.platforms.common.workflow;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -10,11 +11,14 @@ import org.eclipse.xtend.expression.AbstractExpressionsUsingWorkflowComponent.Gl
 
 /**
  * Helper class for other workflow components
- * @author Aibek Isaev
- * @author Beishen
+ * 
+ * @author 	Aibek Isaev
+ * @author 	Beishen
+ * @author 	Nico Herbig <nico.herbig@genesez.org>
+ * @date 	2011-04-13
  */
 public class WorkflowUtils {
-	
+
 	public static final Properties defaults = new Properties();
 	static {
 		defaults.put("slot", "coremodel");
@@ -25,12 +29,14 @@ public class WorkflowUtils {
 		defaults.put("greqPackage", "de.genesez.metamodel.greq.GreqPackage");
 		defaults.put("gtracePackage", "de.genesez.metamodel.gtrace.GtracePackage");
 	}
-	
+
 	/**
 	 * Method for create GlobalVarDef
 	 * 
-	 * @param name	GlobalVarDef's name
-	 * @param value	GlobalVarDef's value. Type is String
+	 * @param name
+	 *            GlobalVarDef's name
+	 * @param value
+	 *            GlobalVarDef's value. Type is String
 	 * @return GLobalVarDef's object
 	 */
 	public static GlobalVarDef createGlobalVarDef(String name, String value) {
@@ -39,12 +45,14 @@ public class WorkflowUtils {
 		GVD.setValue(value);
 		return GVD;
 	}
-	
+
 	/**
 	 * Method for create GlobalVarDef
 	 * 
-	 * @param name	GlobalVarDef's name
-	 * @param value	GlobalVarDef's value. Type is boolean
+	 * @param name
+	 *            GlobalVarDef's name
+	 * @param value
+	 *            GlobalVarDef's value. Type is boolean
 	 * @return GLobalVarDef's object
 	 */
 	public static GlobalVarDef createGlobalVarDef(String name, boolean value) {
@@ -53,12 +61,13 @@ public class WorkflowUtils {
 		GVD.setValue(Boolean.toString(value));
 		return GVD;
 	}
-	
+
 	/**
 	 * splits a comma or semicolon separated list into a list an trims the
 	 * values
 	 * 
-	 * @param list	a comma or semicolon separated list
+	 * @param list
+	 *            a comma or semicolon separated list
 	 * @return a list of strings
 	 */
 	public static List<String> split(String list) {
@@ -70,11 +79,12 @@ public class WorkflowUtils {
 		}
 		return result;
 	}
-	
+
 	/**
 	 * Convert from Array to String
 	 * 
-	 * @param name	of List<String>
+	 * @param name
+	 *            of List<String>
 	 * @return String
 	 */
 	public static String arrayToString(List<String> name) {
@@ -85,4 +95,44 @@ public class WorkflowUtils {
 		}
 		return s.toString();
 	}
+
+	public static void setDefaultProperties(Object workflowComponent, Properties defaults) {
+		String property;
+		String value;
+		Method invokeMethod;
+
+		for (Object keyObj : defaults.keySet()) {
+			property = String.valueOf(keyObj);
+			value = defaults.getProperty(property);
+
+			try {
+				invokeMethod = getPropertyAccessorMethod(workflowComponent, property);
+				invokeMethod.invoke(workflowComponent, value);
+			} catch (Throwable e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static Method getPropertyAccessorMethod(Object workflowComponent, String property) throws Throwable {
+		String methodName;
+		Method method;
+
+		methodName = property.substring(0, 1).toUpperCase() + property.substring(1);
+
+		// Trying to get the setter method of property
+		try {
+			method = workflowComponent.getClass().getMethod("set" + methodName, String.class);
+		} catch (Throwable e) {
+
+			// If there is no setter, trying to get the adder method of property
+			try {
+				method = workflowComponent.getClass().getMethod("add" + methodName, String.class);
+			} catch (Throwable e1) {
+				throw new Throwable("Could not find set or add method for property: " + property);
+			}
+		}
+		return method;
+	}
+
 }
