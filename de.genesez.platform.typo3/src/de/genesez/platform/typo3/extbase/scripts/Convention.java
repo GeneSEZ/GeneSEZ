8f4286eb1326968da9dd820214fdfeff46264f20
Nico Herbig <nico@nicoherbig.io>
2011-07-19 15:56:06 +0000

    Refactor workflow concept. Do rework for localization and naming. Start generation of TCA

--- de.genesez.platform.typo3/src/de/genesez/platform/typo3/extbase/scripts/Convention.java
+++ de.genesez.platform.typo3/src/de/genesez/platform/typo3/extbase/scripts/Convention.java
@@ -1,18 +1,34 @@
 package de.genesez.platform.typo3.extbase.scripts;
 
-public class Convention {
+import de.genesez.metamodel.gcore.MProperty;
 
+public class Convention {
+	
 	public static String extensionName(String extensionKey) {
 		StringBuffer extensionName = new StringBuffer();
-
+		
 		for (String extensionKeyPart : extensionKey.split("_")) {
-			extensionName.append( toUpperCamelCase(extensionKeyPart) );
+			extensionName.append(toUpperCamelCase(extensionKeyPart));
 		}
 		return extensionName.toString();
+	}
+	
+	public static String toLowerPropertyName(MProperty property) {
+		StringBuffer propertyName = new StringBuffer();
+		String[] propertyNameParts;
+		
+		propertyNameParts = property.getName().split("(?=[A-Z])");
+		for (int i = 0; i < propertyNameParts.length; i++) {
+			propertyName.append(propertyNameParts[i].toLowerCase());
+			if (i < propertyNameParts.length - 1) {
+				propertyName.append("_");
+			}
+		}
+		return propertyName.toString();
 	}
 	
 	private static String toUpperCamelCase(String str) {
 		return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
 	}
-
+	
 }
