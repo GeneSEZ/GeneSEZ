77460bb9f6b60deaf09bc4a032ff2dfc8189b8ee
nihe <nihe@a19731f8-c322-0410-8b9b-f98442d78366>
2011-07-20 20:10:46 +0000

    Restructure TYPO3 Extbase platform

---
+++ trunk/de.genesez.platform.typo3/src/de/genesez/platform/typo3/extbase/convention/Conversion.java
@@ -0,0 +1,11 @@
+package de.genesez.platform.typo3.extbase.convention;
+
+/**
+ * Utility class for TYPO3 Extbase extension conversions of model information.
+ * 
+ * @author Nico Herbig <nico.herbig@fh-zwickau.de>
+ * @date 2011-04-13
+ */
+public class Conversion {
+	
+}
