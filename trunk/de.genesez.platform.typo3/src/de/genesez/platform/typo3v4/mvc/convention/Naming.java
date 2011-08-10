6d5e46fd4e80c4a6a9ca437ae5b8df6146e9d037
nihe <nihe@a19731f8-c322-0410-8b9b-f98442d78366>
2011-08-10 17:27:24 +0000

    Complete refactoring of TYPO3 platform. Now, Extbase and Fluid are in the same package 'typo3v4-mvc' like the project name at TYPO3.org.

---
+++ trunk/de.genesez.platform.typo3/src/de/genesez/platform/typo3v4/mvc/convention/Naming.java
@@ -0,0 +1,152 @@
+package de.genesez.platform.typo3v4.mvc.convention;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.genesez.metamodel.gcore.MElement;
+import de.genesez.metamodel.gcore.MModel;
+import de.genesez.metamodel.gcore.MOperation;
+import de.genesez.metamodel.gcore.MProperty;
+import de.genesez.platforms.common.AccessHelper;
+
+/**
+ * Utility class for TYPO3 Extbase naming conventions.
+ * 
+ * @author Nico Herbig <nico.herbig@fh-zwickau.de>
+ * @date 2011-07-28
+ */
+public class Naming {
+	
+	/**
+	 * Stores the previous naming context for naming model elements.
+	 */
+	public static String previousNamingContext;
+	
+	/**
+	 * Stores the actual naming context for naming model elements.
+	 */
+	public static String actualNamingContext;
+	
+	/**
+	 * Get the actual naming context which is stored in the static variable
+	 * 'actualNamingContext'.
+	 * 
+	 * @return The actual naming context
+	 */
+	public static String getNamingContext() {
+		return Naming.actualNamingContext;
+	}
+	
+	/**
+	 * Set the new naming context which is stored in the static variable
+	 * 'actualNamingContext'.
+	 * 
+	 * @param namingContext The new naming context
+	 */
+	public static void setNamingContext(String namingContext) {
+		Naming.previousNamingContext = Naming.actualNamingContext;
+		Naming.actualNamingContext = namingContext;
+	}
+	
+	/**
+	 * Revert the previous naming context which is stored in the static variable
+	 * 'previousNamingContext'.
+	 */
+	public static void revertNamingContext() {
+		Naming.actualNamingContext = Naming.previousNamingContext;
+	}
+	
+	/**
+	 * This method is for all attributes and association roles of a domain
+	 * object. It converts the property name to get an valid property name in
+	 * configuration and localization environment. It splits the property name
+	 * by every capital letter and transform every token to lower case and add
+	 * underscores between of them. For instance: the property name is
+	 * 'relatedPosts' it converts to 'related_posts'.
+	 * 
+	 * @param property The property
+	 * @return The property name in lower case and with underscores
+	 */
+	public static String toLowerPropertyName(MProperty property) {
+		StringBuffer propertyName = new StringBuffer();
+		String[] propertyNameParts;
+		
+		propertyNameParts = property.getName().split("(?=[A-Z])");
+		for (int i = 0; i < propertyNameParts.length; i++) {
+			propertyName.append(propertyNameParts[i].toLowerCase());
+			if (i < propertyNameParts.length - 1) {
+				propertyName.append("_");
+			}
+		}
+		return propertyName.toString();
+	}
+	
+	/**
+	 * This method convert assigned controller actions of an TYPO3 Extbase
+	 * plugin or module. In every plugin or module stereotype it is possible to
+	 * store a list of methods for cached and uncached controller actions. This
+	 * action names are transformed into xmiGuids that are only strings. For a
+	 * correct controller action combination it is necessary to get a reference
+	 * to the domain object where the action is owned. So this method gets a
+	 * list of xmiGuids of cached or uncached actions. With this information it
+	 * is possible to iterate over the model tree and get a reference back to
+	 * the operation object. In fact that the operation has a reference to the
+	 * classifier it is possible to get the name of this to create valid
+	 * controller action pair. For instance: The plugin needs the following
+	 * controller actions 'BlogController' -> 'indexAction, listAction' and
+	 * 'PostController' -> 'indexAction'. This method returns a string list with
+	 * two entries: [0] : 'Blog' => 'index, list' and [1] : 'Post' => 'index'.
+	 * 
+	 * @param model The model for iteration over it
+	 * @param xmiGuids The list of operation xmiGuids
+	 * @return The list of valid controller actions
+	 */
+	public static List<String> asControllerActionPairs(MModel model, List<String> xmiGuids) {
+		List<String> controllerActions = new ArrayList<String>();
+		List<String> controllers = new ArrayList<String>();
+		List<String> actions = new ArrayList<String>();
+		String controller;
+		Integer controllerIndex;
+		String action;
+		
+		for (String xmiGuid : xmiGuids) {
+			// Get the reference of an element and check of this element is an operation
+			MElement elem = AccessHelper.getMElement(model, xmiGuid);
+			if (elem instanceof MOperation) {
+				// Extract the classifier name where the operation is owned
+				controller = ((MOperation) elem).getClassifier().getName();
+				// Extract the operation name
+				action = ((MOperation) elem).getName();
+				
+				// Rewrite the controller name
+				if (controller.endsWith("Controller")) {
+					controller = controller.substring(0, controller.lastIndexOf("Controller"));
+				}
+				
+				// Rewrite the action name
+				if (action.endsWith("Action")) {
+					action = action.substring(0, action.lastIndexOf("Action"));	
+				}
+								
+				// Check if the controller already exists inside the controller list or not
+				if (!controllers.contains(controller)) {
+					// If not a new controller and action entry is added
+					controllers.add(controller);
+					actions.add(action);
+				} else {
+					// Otherwise the index of the existing controller is searched
+					controllerIndex = controllers.indexOf(controller);
+					// At the same index the new action is added
+					actions.set(controllerIndex, actions.get(controllerIndex) + ", " + action);
+				}
+			}
+		}
+		
+		// This loop transforms controllers and actions to be valid TYPO3 Extbase controller action pairs.
+		for (int i = 0; i < controllers.size(); i++) {
+			controllerActions.add("'" + controllers.get(i) + "' => '" + actions.get(i) + "'");
+		}
+		return controllerActions;
+	}
+	
+}
