5d83800e9d5a6150bc3505e0d23ab3e50c02a0ed
nihe <nihe@a19731f8-c322-0410-8b9b-f98442d78366>
2011-11-25 14:07:33 +0000

    Refactor algorithm to get an object by it's xmiGuid. Now the resource map of EMF is used instead of iteration over the whole model.

--- trunk/de.genesez.platforms.common/src/de/genesez/platforms/common/AccessHelper.java
+++ trunk/de.genesez.platforms.common/src/de/genesez/platforms/common/AccessHelper.java
@@ -2,11 +2,10 @@ package de.genesez.platforms.common;
 
 import java.util.List;
 
-import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.common.util.WrappedException;
 import org.eclipse.emf.ecore.EObject;
 
 import de.genesez.metamodel.gcore.MClassifier;
-import de.genesez.metamodel.gcore.MElement;
 import de.genesez.metamodel.gcore.MModel;
 import de.genesez.metamodel.gcore.MPackage;
 
@@ -24,21 +23,21 @@ import de.genesez.metamodel.gcore.MPackage;
 public class AccessHelper {
 
 	/**
-	 * Getter for an MElement by it's xmi guid.
+	 * Getter for an EObject by it's URI. 
+	 * In case of an MElement the URI is normally the xmiGuid.
 	 * 
-	 * @param model the model where the element is placed
-	 * @param xmiGuid the xmi guid of the element
-	 * @return the object where the xmi guid is equals, otherwise null
+	 * @param model the model where the object is placed
+	 * @param uri the URI of the object
+	 * @return the object where the URI is equals, otherwise null
 	 */
-	public static MElement getMElement(MModel model, String xmiGuid) {
-		TreeIterator<EObject> it;
+	public static EObject getEObjectByUri(MModel model, String uri) {
+		EObject eObj;
 
-		it = model.eAllContents();
-		while (it.hasNext()) {
-			EObject eObj = (EObject) it.next();
-			if (eObj instanceof MElement && xmiGuid.equals(getXmiGuid(eObj))) {
-				return (MElement) eObj;
-			}
+		try {
+			eObj = model.eResource().getEObject(uri);
+			if (eObj instanceof EObject)
+				return eObj;
+		} catch (WrappedException e) {
 		}
 		return null;
 	}
@@ -109,26 +108,6 @@ public class AccessHelper {
 			}
 		}
 		return pSearchedFor;
-	}
-
-	/**
-	 * Getter for the xmi guid of an element.
-	 * 
-	 * Inside this function a type case from EObject to MElement is tryed. If
-	 * the element is not an MElement then the string representation is
-	 * returned.
-	 * 
-	 * @param eObj an eobject
-	 * @return the xmi guid, if parameter eObj is an MElement, otherwise the
-	 *         string representation of eObj
-	 */
-	private static String getXmiGuid(EObject eObj) {
-		// note: if EObject is no MElement then there is no xmi guid.
-		try {
-			return ((MElement) eObj).getXmiGuid();
-		} catch (ClassCastException cce) {
-			return eObj.toString();
-		}
 	}
 
 	/**
