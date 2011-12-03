ce1ab2b2e8929755cfb68e4293bbee2b2f934cc5
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2011-12-03 21:39:49 +0000

    refactored the global store to allow storage of many arbitrary objects

--- de.genesez.platforms.common/src/de/genesez/platforms/common/GlobalStore.java
+++ de.genesez.platforms.common/src/de/genesez/platforms/common/GlobalStore.java
@@ -1,34 +1,51 @@
 package de.genesez.platforms.common;
 
-import de.genesez.metamodel.gcore.MModel;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
- * Utility class for storing a model
+ * Utility class for storing model elements and objects.
  * 
- * @author	nihe
- * @author	geobe
- * @date	2008-04-18
+ * @author nihe
+ * @author geobe
+ * @author tobias haubold (last)
+ * @date 2008-04-18
  */
 public class GlobalStore {
 	
 	/**
-	 * holds a model reference
+	 * storage backend
 	 */
-	private static MModel model;
+	private static Map<String, Object> safe = new LinkedHashMap<String, Object>();
 	
 	/**
-	 * getter for the model reference
-	 * @return	the stored model reference
+	 * @return the value of the specified key.
 	 */
-	public static MModel getModel() {
-		return model;
+	public static Object get(String key) {
+		return safe.get(key);
 	}
 	
 	/**
-	 * setter for the model reference
-	 * @param mod	model reference to save
+	 * Saves a key-value pair.
+	 * @param key	the key to access the value
+	 * @param value	the value to store
 	 */
-	public static void setModel(MModel mod) {
-		model = mod;
+	public static Object save(String key, Object value) {
+		return safe.put(key, value);
+	}
+	
+	/**
+	 * Removes the key and it's value.
+	 * @param key	the key to access a value
+	 */
+	public static Object remove(String key) {
+		return safe.remove(key);
+	}
+	
+	/**
+	 * Clears all saved objects.
+	 */
+	public static void clear() {
+		safe.clear();
 	}
 }
