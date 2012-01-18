595d4fd9a8e2e9eea5f7c065a09f0bb53755abaa
domwet <domwet@a19731f8-c322-0410-8b9b-f98442d78366>
2012-01-18 12:17:39 +0000

    The renamed and generated commons project. Tried to make changes with the svn commands, but I think it didn't work for every file. Also all scripts and Templates are renamed so make sure to check your own scripts if they depend on those.
    Also changed slightly things in TypeMapper:
    Now it's possible to set a typeMappingFile as an absolute path (needed for the Starter in org.genesez.tools.profiling)

---
+++ de.genesez.platforms.common/test/org/genesez/platforms/common/typemapping/UnitTypeMapper.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.genesez.platforms.common.typemapping;
+
+import org.genesez.platforms.common.typemapping.TypeMapper;
+
+/**
+ * @author pethu
+ *
+ */
+public class UnitTypeMapper extends TypeMapper {
+
+	/**
+	 * 
+	 */
+	public UnitTypeMapper(String...strings ) {
+		super(strings);
+	}
+
+}
