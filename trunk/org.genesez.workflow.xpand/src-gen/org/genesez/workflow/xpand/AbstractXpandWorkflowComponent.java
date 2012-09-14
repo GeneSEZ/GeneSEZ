19f5cb9f72b48bb50a29bb7a48c0b0ddb4e188e1
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2012-09-14 14:45:41 +0000

    added emf meta model package parameter to simplify meta model registration

--- trunk/org.genesez.workflow.xpand/src-gen/org/genesez/workflow/xpand/AbstractXpandWorkflowComponent.java
+++ trunk/org.genesez.workflow.xpand/src-gen/org/genesez/workflow/xpand/AbstractXpandWorkflowComponent.java
@@ -10,6 +10,7 @@ import static org.genesez.workflow.profile.WorkflowFileInclusion.WHEN_NEEDED;
 import org.eclipse.emf.mwe.core.issues.Issues;
 import org.eclipse.xtend.expression.AbstractExpressionsUsingWorkflowComponent.GlobalVarDef;
 import org.eclipse.xtend.typesystem.MetaModel;
+import org.eclipse.xtend.typesystem.emf.EmfMetaModel;
 import org.genesez.workflow.AbstractSlotWorkflowComponent;
 import org.genesez.workflow.profile.Parameter;
 import java.lang.annotation.Documented;
@@ -29,11 +30,14 @@ public abstract class AbstractXpandWorkflowComponent extends AbstractSlotWorkflo
 	@Parameter(isRequired = false, isMultiValued = true, workflowInclusion = ALWAYS)
 	private java.util.Set<String> aopScript = new java.util.LinkedHashSet<String>();
 	
-	@Parameter(isRequired = true, isMultiValued = true, workflowInclusion = WHEN_NEEDED)
+	@Parameter(isRequired = false, isMultiValued = true, workflowInclusion = WHEN_NEEDED)
 	private java.util.Set<MetaModel> metaModel = new java.util.LinkedHashSet<MetaModel>();
 	
 	@Parameter(isRequired = false, isMultiValued = true, workflowInclusion = WHEN_NEEDED)
 	private java.util.Set<GlobalVarDef> globalVarDef = new java.util.LinkedHashSet<GlobalVarDef>();
+	
+	@Parameter(isRequired = false, isMultiValued = true, workflowInclusion = WHEN_NEEDED)
+	private java.util.Set<String> emfMetaModelPackage = new java.util.HashSet<String>();
 	
 	// -- generated method stubs for implementations + derived attributes ---
 	/**
@@ -42,10 +46,45 @@ public abstract class AbstractXpandWorkflowComponent extends AbstractSlotWorkflo
 	 */
 	public void checkConfiguration(Issues issues) {
 		/* PROTECTED REGION ID(java.implementation._jVgogPqdEeGS1KtqERYUjQ) ENABLED START */
+		if (!emfMetaModelPackage.isEmpty()) {
+			for (String p : emfMetaModelPackage) {
+				EmfMetaModel emm = new EmfMetaModel();
+				emm.setMetaModelPackage(p);
+				metaModel.add(emm);
+			}
+		}
 		if (metaModel.isEmpty()) {
-			issues.addError(this, "Workflow parameter 'metaModel' is missing!", metaModel);
+			issues.addError(this, "Workflow parameter 'metaModel' or 'emfMetaModelPackage' is missing!", metaModel);
 		}
 		super.checkConfiguration(issues);
+		/* PROTECTED REGION END */
+	}
+	
+	/**
+	 * Method stub for further implementation.
+	 * @param	name	
+	 * @param	value	
+	 */
+	public void addGlobalVarDef(String name, String value) {
+		/* PROTECTED REGION ID(java.implementation._uiyqUP4AEeGA35ujkRyC6w) ENABLED START */
+		GlobalVarDef gvd = new GlobalVarDef();
+		gvd.setName(name);
+		gvd.setValue("'" + value + "'");
+		globalVarDef.add(gvd);
+		/* PROTECTED REGION END */
+	}
+	
+	/**
+	 * Method stub for further implementation.
+	 * @param	name	
+	 * @param	value	
+	 */
+	public void addGlobalVarDef(String name, Object value) {
+		/* PROTECTED REGION ID(java.implementation._v14C0P4AEeGA35ujkRyC6w) ENABLED START */
+		GlobalVarDef gvd = new GlobalVarDef();
+		gvd.setName(name);
+		gvd.setValue(String.valueOf(value));
+		globalVarDef.add(gvd);
 		/* PROTECTED REGION END */
 	}
 	
@@ -119,6 +158,29 @@ public abstract class AbstractXpandWorkflowComponent extends AbstractSlotWorkflo
 		this.globalVarDef.remove(globalVarDef);
 	}
 	
+	/**
+	 * Returns the value of attribute '<em><b>emfMetaModelPackage</b></em>'
+	 */
+	public java.util.Set<String> getEmfMetaModelPackage() {
+		return emfMetaModelPackage;
+	}
+	
+	/**
+	 * Adds the specified value to the attribute '<em><b>emfMetaModelPackage</b></em>'.
+	 * @param	emfMetaModelPackage	the value to add
+	 */
+	public void addEmfMetaModelPackage(String emfMetaModelPackage) {
+		this.emfMetaModelPackage.add(emfMetaModelPackage);
+	}
+	
+	/**
+	 * Removes the specified value from the attribute '<em><b>emfMetaModelPackage</b></em>'.
+	 * @param	emfMetaModelPackage	the value to remove
+	 */
+	public void removeEmfMetaModelPackage(String emfMetaModelPackage) {
+		this.emfMetaModelPackage.remove(emfMetaModelPackage);
+	}
+	
 	// -- generated code  ---------------------------------------------------
 	
 	// -- own code implementation -------------------------------------------
@@ -182,6 +244,26 @@ public abstract class AbstractXpandWorkflowComponent extends AbstractSlotWorkflo
 		
 		// -- own code implementation -------------------------------------------
 		/* PROTECTED REGION ID(java.annotation.own.code.declaration._t_Z00PqcEeGS1KtqERYUjQ_annotation) ENABLED START */
+		/* PROTECTED REGION END */
+	}
+	
+	/**
+	 * Please describe the responsibility of your class in your modeling tool.
+	 * @author dreamer
+	 */
+	@Documented
+	@Target({ TYPE })
+	@Retention(RUNTIME)
+	public @interface WpdEmfMetaModelPackage {
+		
+		// -- generated elements ------------------------------------------------
+		String[] value();
+		
+		// -- generated code of other cartridges --------------------------------
+		
+		// -- own code implementation -------------------------------------------
+		/* PROTECTED REGION ID(java.annotation.own.code.declaration._8F2lgP3mEeGA35ujkRyC6w_annotation) ENABLED START */
+		// TODO: put your own implementation code here
 		/* PROTECTED REGION END */
 	}
 	
