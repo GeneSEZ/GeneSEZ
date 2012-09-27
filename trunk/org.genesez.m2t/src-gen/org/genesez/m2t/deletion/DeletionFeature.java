58369deaf2054c41884920c141a3dd6e22ffed68
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2012-09-27 10:47:45 +0000

    added org.genesez.m2t model + transforms + project as the new place for file tree walker, file + folder deletion and import take over

---
+++ trunk/org.genesez.m2t/src-gen/org/genesez/m2t/deletion/DeletionFeature.java
@@ -0,0 +1,105 @@
+package org.genesez.m2t.deletion;
+
+/* 
+ *	Do not place import/include statements above this comment, just below. 
+ * 	@FILE-ID : (_ZAIqzgD0EeK7ac-mrkJBDw) 
+ */
+
+import org.genesez.m2t.FileTreeObserverAdapter;
+
+/**
+ * Superclass for the FileDeletion. Contains methods to check which repository is in use and logs them.
+ * 
+ * @author Dominik Wetzel
+ * @date 2011-10-11
+ * @author dreamer
+ */
+public abstract class DeletionFeature extends FileTreeObserverAdapter {
+	
+	// -- generated attribute, constant + association declarations ----------
+	
+	/**
+	 * Directory names that are excluded during file search
+	 */
+	protected java.util.Set<String> excludedDirectoryNames = new java.util.HashSet<String>();
+	
+	/**
+	 * paths relative to output directory that are excluded during the file search.
+	 */
+	protected java.util.Set<String> excludedRelativePaths = new java.util.HashSet<String>();
+	
+	/**
+	 * switch for deletion
+	 */
+	protected boolean enabled = true;
+	
+	/**
+	 * trigger for the NotPreparedException
+	 */
+	protected boolean prepared = false;
+	
+	// -- generated method declarations -------------------------------------
+	/**
+	 * Method stub for further implementation.
+	 */
+	public abstract void delete();
+	
+	// -- generated method stubs for implementations + derived attributes ---
+	/**
+	 * Sets prepared to true. Called if file tree walked completely.
+	 */
+	public void updateComplete() {
+		/* PROTECTED REGION ID(java.implementation._ZAIqwgD0EeK7ac-mrkJBDw) ENABLED START */
+		prepared = true;
+		/* PROTECTED REGION END */
+	}
+	
+	// -- generated association + attribute accessors -----------------------
+	/**
+	 * Directory names that are excluded during file search
+	 */
+	public void addExcludedDirectoryNames(String excludedDirectoryNames) {
+		this.excludedDirectoryNames.add(excludedDirectoryNames);
+	}
+	
+	/**
+	 * Directory names that are excluded during file search
+	 */
+	public void removeExcludedDirectoryNames(String excludedDirectoryNames) {
+		this.excludedDirectoryNames.remove(excludedDirectoryNames);
+	}
+	
+	/**
+	 * paths relative to output directory that are excluded during the file search.
+	 */
+	public void addExcludedRelativePaths(String excludedRelativePaths) {
+		this.excludedRelativePaths.add(excludedRelativePaths);
+	}
+	
+	/**
+	 * paths relative to output directory that are excluded during the file search.
+	 */
+	public void removeExcludedRelativePaths(String excludedRelativePaths) {
+		this.excludedRelativePaths.remove(excludedRelativePaths);
+	}
+	
+	/**
+	 * switch for deletion
+	 */
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+	// -- generated code  ---------------------------------------------------
+	
+	// -- own code implementation -------------------------------------------
+	/* PROTECTED REGION ID(java.class.own.code.implementation._ZAIqzgD0EeK7ac-mrkJBDw) ENABLED START */
+	{
+		// exclude all revision control metadata folders
+		for (RevisionControlSystem rep : RevisionControlSystem.REGISTRY) {
+			excludedDirectoryNames.add(rep.getMetadataFolderName());
+		}
+	}
+	/* PROTECTED REGION END */
+	
+}
---
+++ trunk/org.genesez.m2t/src-gen/org/genesez/m2t/deletion/SubversionRcs.java
@@ -0,0 +1,90 @@
+package org.genesez.m2t.deletion;
+
+/* 
+ *	Do not place import/include statements above this comment, just below. 
+ * 	@FILE-ID : (_bP3SQAS9EeKUpcpqfZ9tsA) 
+ */
+
+import java.io.File;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.wc.SVNClientManager;
+import org.tmatesoft.svn.core.wc.SVNWCClient;
+
+/**
+ * Implementation of the delete algorithm for SVN.
+ * 
+ * @author Dominik Wetzel
+ * @date 2011-09-15
+ * @author dreamer
+ */
+public class SubversionRcs implements RevisionControlSystem {
+	
+	// -- generated attribute, constant + association declarations ----------
+	
+	private String metadataFolderName = ".svn";
+	
+	private SVNClientManager manager = SVNClientManager.newInstance();
+	
+	private SVNWCClient wcClient = manager.getWCClient();
+	
+	// -- generated method stubs for implementations + derived attributes ---
+	/**
+	 * gives the metadata foldername of the currently checked RCS.
+	 * @return	the metadata folder name
+	 */
+	public String getMetadataFolderName() {
+		/* PROTECTED REGION ID(java.implementation._bP3SQAS9EeKUpcpqfZ9tsA__qUp80gQdEeK-ufRtI9UaTA) ENABLED START */
+		return metadataFolderName;
+		/* PROTECTED REGION END */
+	}
+	
+	/**
+	 * a callback from the DeletionFeature to set the Path of the metadata folder
+	 * @param	name	the directory where the metadata folder was found
+	 */
+	public void setRepositoryRoot(String name) {
+		/* PROTECTED REGION ID(java.implementation._bP3SQAS9EeKUpcpqfZ9tsA__qUp81gQdEeK-ufRtI9UaTA) ENABLED START */
+		/* PROTECTED REGION END */
+	}
+	
+	/**
+	 * deletes a file with the given path String
+	 * @param	file	the file, that should be deleted
+	 */
+	public void markForDelete(String file) {
+		/* PROTECTED REGION ID(java.implementation._bP3SQAS9EeKUpcpqfZ9tsA__qUp82gQdEeK-ufRtI9UaTA) ENABLED START */
+		try {
+			File path = new File(file);
+			if (path.isDirectory()) {
+				// doDelete(the file, force deletion, delete also on filesystem,
+				// only to test);
+				wcClient.doDelete(path, true, true, false);
+			} else {
+				// doDelete(the file, force deletion, delete also on filesystem,
+				// only to test);
+				wcClient.doDelete(path, true, false, false);
+			}
+		} catch (SVNException e) {
+		}
+		/* PROTECTED REGION END */
+	}
+	
+	/**
+	 * The name that will be printed on the log
+	 * @return	name of the RCS
+	 */
+	public String toString() {
+		/* PROTECTED REGION ID(java.implementation._bP3SQAS9EeKUpcpqfZ9tsA__qUp83gQdEeK-ufRtI9UaTA) ENABLED START */
+		return "Subversion";
+		/* PROTECTED REGION END */
+	}
+	
+	// -- generated association + attribute accessors -----------------------
+	
+	// -- generated code  ---------------------------------------------------
+	
+	// -- own code implementation -------------------------------------------
+	/* PROTECTED REGION ID(java.class.own.code.implementation._bP3SQAS9EeKUpcpqfZ9tsA) ENABLED START */
+	/* PROTECTED REGION END */
+	
+}
