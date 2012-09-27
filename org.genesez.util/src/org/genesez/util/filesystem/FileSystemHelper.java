88fe7a133b93f58b09f7a4a98357ba1b89b1a5b2
Nico Herbig <nico@nicoherbig.io>
2012-09-27 15:00:21 +0000

    update documentation

--- org.genesez.util/src/org/genesez/util/filesystem/FileSystemHelper.java
+++ org.genesez.util/src/org/genesez/util/filesystem/FileSystemHelper.java
@@ -17,7 +17,7 @@ import java.io.File;
 import java.nio.file.attribute.PosixFilePermission;
 
 /**
- * Helper class to create file system elements 
+ * Helper class to create file system elements.
  * 
  * @author nicher
  */
@@ -25,9 +25,9 @@ public class FileSystemHelper {
 	
 	// -- generated method stubs for implementations + derived attributes ---
 	/**
-	 * Creates the directory named by this abstract directory name.
-	 * @param	directoryName	the directory name
-	 * @return	true if and only if the directory was created, otherwise false
+	 * Creates a directory with the specified directory name.
+	 * @param	directoryName	The directory name.
+	 * @return	True if and only if the directory was created, otherwise false.
 	 */
 	public static boolean createDirectory(String directoryName) {
 		/* PROTECTED REGION ID(java.implementation._17_0_1_8e00291_1324545009733_326318_2002) ENABLED START */
@@ -46,14 +46,15 @@ public class FileSystemHelper {
 	}
 	
 	/**
-	 * Creates the directory named by this abstract path name, including any necessary but nonexistent parent directories.
-	 * @param	pathName	the path name
-	 * @return	true if and only if the directory was created, along with all necessary parent directories, otherwise false
+	 * Creates a directory with the specified directory path name, including all necessary but nonexistent parent directories.
+	 *
+	 * @param	directoryPathName	The directory path name.
+	 * @return	True if and only if the directory was created, along with all necessary parent directories, otherwise false.
 	 */
-	public static boolean createDirectoryPath(String pathName) {
+	public static boolean createDirectoryPath(String directoryPathName) {
 		/* PROTECTED REGION ID(java.implementation._17_0_1_8e00291_1324545416605_447454_2019) ENABLED START */
 		boolean retVal = false;
-		File directory = new File(pathName);
+		File directory = new File(directoryPathName);
 		
 		// checks if file (directory) not exists
 		if (!directory.exists()) {
