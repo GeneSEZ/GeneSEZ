75d7f2a5c8d0d27be75e273939e64470ba5128f6
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2012-10-04 12:09:02 +0000

    added reference to typemapping, registered typemapping files, added model2text composite component

---
+++ org.genesez.workflow.xpand/src-gen/org/genesez/workflow/xpand/Model2TextComposite.java
@@ -0,0 +1,227 @@
+package org.genesez.workflow.xpand;
+
+/* 
+ *	Do not place import/include statements above this comment, just below. 
+ * 	@FILE-ID : (_T90ZMAWHEeKiOsNmFSLL5Q) 
+ */
+
+import java.util.Set;
+import org.genesez.m2t.deletion.RevisionControlSystemFinder;
+import org.genesez.m2t.deletion.FileDeletion;
+import org.genesez.m2t.deletion.FolderDeletion;
+import org.eclipse.emf.mwe.core.WorkflowContext;
+import org.eclipse.emf.mwe.core.monitor.ProgressMonitor;
+import org.eclipse.emf.mwe.core.issues.Issues;
+import org.genesez.workflow.CompositeComponent;
+import org.genesez.m2t.xpand.XpandImportPreserver;
+import org.genesez.m2t.FileTreeObserver;
+import org.genesez.m2t.FileTreeWalker;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.Log;
+
+/**
+ * Please describe the responsibility of your class in your modeling tool.
+ * @author dreamer
+ */
+public class Model2TextComposite<T extends Model2Text> extends CompositeComponent<T> {
+	
+	// -- generated attribute, constant + association declarations ----------
+	
+	public final Log logger = LogFactory.getLog(getClass());
+	
+	private RevisionControlSystemFinder revisionControlSystemFinder;
+	
+	private FileDeletion fileDeletion;
+	
+	private FolderDeletion folderDeletion;
+	
+	private XpandImportPreserver importPreserver;
+	
+	private FileTreeWalker preFileTreeWalker;
+	
+	private FileTreeWalker postFileTreeWalker;
+	
+	// -- generated method stubs for implementations + derived attributes ---
+	/**
+	 * Invokes the execution of the component.
+	 * @param	context	The context of workflow execution.
+	 * @param	monitor	Instance to report any activity to.
+	 * @param	issues	Instance to collect all problems during execution.
+	 */
+	protected void invokeInternal(WorkflowContext context, ProgressMonitor monitor, Issues issues) {
+		/* PROTECTED REGION ID(java.implementation._ylbdEAXGEeKdMrzRF_vTbg) ENABLED START */
+		preFileTreeWalker.walkFileTree();
+		super.invokeInternal(context, monitor, issues);
+		fileDeletion.delete();
+		postFileTreeWalker.walkFileTree();
+		folderDeletion.delete();
+		/* PROTECTED REGION END */
+	}
+	
+	/**
+	 * Validates the configuration of the component before invocation.
+	 * @param	issues	Instance to collect all problems during configuration check.
+	 */
+	public void checkConfiguration(Issues issues) {
+		/* PROTECTED REGION ID(java.implementation._wstA4AXGEeKdMrzRF_vTbg) ENABLED START */
+		// the default output directory from the first model2text component
+		String defaultOutputDir = null;
+		if (!getComponent().isEmpty()) {
+			defaultOutputDir = getComponent().iterator().next().getOutputDir();
+		}
+		
+		// check configuration
+		if (fileDeletion == null) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Use default file deletion configuration.");
+			}
+			fileDeletion = new FileDeletion();
+		}
+		if (folderDeletion == null) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Use default folder deletion configuration.");
+			}
+			folderDeletion = new FolderDeletion();
+		}
+		if (revisionControlSystemFinder == null) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Use default revision control system finder.");
+			}
+			revisionControlSystemFinder = new RevisionControlSystemFinder();
+			revisionControlSystemFinder.setBaseDir(defaultOutputDir);
+		}
+		if (preFileTreeWalker == null) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Use default pre file tree walker configuration.");
+			}
+			preFileTreeWalker = new FileTreeWalker();
+			preFileTreeWalker.setBaseDir(defaultOutputDir);
+			preFileTreeWalker.addObserver(revisionControlSystemFinder);
+			preFileTreeWalker.addObserver(fileDeletion);
+			if (importPreserver != null)
+				preFileTreeWalker.addObserver(importPreserver);
+		} else {
+			Set<FileTreeObserver> obs = preFileTreeWalker.getObserver();
+			if (!obs.contains(revisionControlSystemFinder))
+				obs.add(revisionControlSystemFinder);
+			if (!obs.contains(folderDeletion))
+				obs.add(folderDeletion);
+			if (importPreserver != null && !obs.contains(importPreserver))
+				obs.add(importPreserver);
+		}
+		if (postFileTreeWalker == null) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("Use default post file tree walker configuration.");
+			}
+			postFileTreeWalker = new FileTreeWalker();
+			postFileTreeWalker.setBaseDir(defaultOutputDir);
+			postFileTreeWalker.addObserver(folderDeletion);
+		} else {
+			Set<FileTreeObserver> obs = postFileTreeWalker.getObserver();
+			if (!obs.contains(folderDeletion))
+				obs.add(folderDeletion);
+		}
+		
+		// add import preserver as post processor
+		if (importPreserver != null) {
+			for (Model2Text m2t : getComponent()) {
+				m2t.addPostProcessor(importPreserver);
+			}
+		}
+		
+		super.checkConfiguration(issues);
+		/* PROTECTED REGION END */
+	}
+	
+	// -- generated association + attribute accessors -----------------------
+	/**
+	 * Returns the value of attribute '<em><b>revisionControlSystemFinder</b></em>'
+	 */
+	public RevisionControlSystemFinder getRevisionControlSystemFinder() {
+		return revisionControlSystemFinder;
+	}
+	
+	/**
+	 * Sets the value of attribute '<em><b>revisionControlSystemFinder</b></em>'
+	 */
+	public void setRevisionControlSystemFinder(RevisionControlSystemFinder revisionControlSystemFinder) {
+		this.revisionControlSystemFinder = revisionControlSystemFinder;
+	}
+	
+	/**
+	 * Returns the value of attribute '<em><b>fileDeletion</b></em>'
+	 */
+	public FileDeletion getFileDeletion() {
+		return fileDeletion;
+	}
+	
+	/**
+	 * Sets the value of attribute '<em><b>fileDeletion</b></em>'
+	 */
+	public void setFileDeletion(FileDeletion fileDeletion) {
+		this.fileDeletion = fileDeletion;
+	}
+	
+	/**
+	 * Returns the value of attribute '<em><b>folderDeletion</b></em>'
+	 */
+	public FolderDeletion getFolderDeletion() {
+		return folderDeletion;
+	}
+	
+	/**
+	 * Sets the value of attribute '<em><b>folderDeletion</b></em>'
+	 */
+	public void setFolderDeletion(FolderDeletion folderDeletion) {
+		this.folderDeletion = folderDeletion;
+	}
+	
+	/**
+	 * Returns the value of attribute '<em><b>importPreserver</b></em>'
+	 */
+	public XpandImportPreserver getImportPreserver() {
+		return importPreserver;
+	}
+	
+	/**
+	 * Sets the value of attribute '<em><b>importPreserver</b></em>'
+	 */
+	public void setImportPreserver(XpandImportPreserver importPreserver) {
+		this.importPreserver = importPreserver;
+	}
+	
+	/**
+	 * Returns the value of attribute '<em><b>preFileTreeWalker</b></em>'
+	 */
+	public FileTreeWalker getPreFileTreeWalker() {
+		return preFileTreeWalker;
+	}
+	
+	/**
+	 * Sets the value of attribute '<em><b>preFileTreeWalker</b></em>'
+	 */
+	public void setPreFileTreeWalker(FileTreeWalker preFileTreeWalker) {
+		this.preFileTreeWalker = preFileTreeWalker;
+	}
+	
+	/**
+	 * Returns the value of attribute '<em><b>postFileTreeWalker</b></em>'
+	 */
+	public FileTreeWalker getPostFileTreeWalker() {
+		return postFileTreeWalker;
+	}
+	
+	/**
+	 * Sets the value of attribute '<em><b>postFileTreeWalker</b></em>'
+	 */
+	public void setPostFileTreeWalker(FileTreeWalker postFileTreeWalker) {
+		this.postFileTreeWalker = postFileTreeWalker;
+	}
+	
+	// -- generated code  ---------------------------------------------------
+	
+	// -- own code implementation -------------------------------------------
+	/* PROTECTED REGION ID(java.class.own.code.implementation._T90ZMAWHEeKiOsNmFSLL5Q) ENABLED START */
+	/* PROTECTED REGION END */
+	
+}
