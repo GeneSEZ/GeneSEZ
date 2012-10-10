d7b59e41df4cb72a84b71fae9674a466d3a51140
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2012-10-10 09:30:59 +0000

    added content preserve base components, file id provider + import preserver

---
+++ org.genesez.m2t/src-gen/org/genesez/m2t/cp/LineContentExtractor.java
@@ -0,0 +1,40 @@
+package org.genesez.m2t.cp;
+
+/* 
+ *	Do not place import/include statements above this comment, just below. 
+ * 	@FILE-ID : (_vjq8gA7rEeKn_eQP-uNBOQ) 
+ */
+
+import java.nio.file.Path;
+
+/**
+ * Please describe the responsibility of your class in your modeling tool.
+ * @author dreamer
+ */
+public interface LineContentExtractor {
+	
+	/**
+	 * Method stub for further implementation.
+	 * @param	file	
+	 * @return	
+	 */
+	public boolean hasExtractableContent(Path file);
+	
+	/**
+	 * Processes the specified line of content.
+	 * @param	line	
+	 */
+	public void extractContent(String line);
+	
+	/**
+	 * Method stub for further implementation.
+	 * @param	file	
+	 */
+	public void finished(boolean file);
+	
+	// -- generated code of other cartridges --------------------------------
+	
+	// -- own code implementation -------------------------------------------
+	/* PROTECTED REGION ID(java.interface.own.code.declaration._vjq8gA7rEeKn_eQP-uNBOQ) ENABLED START */
+	/* PROTECTED REGION END */
+}
