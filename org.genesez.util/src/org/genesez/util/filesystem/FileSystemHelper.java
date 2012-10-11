5684b140ec0e37a86daa61329befc74382f4734d
Nico Herbig <nico@nicoherbig.io>
2012-10-11 16:40:32 +0000

    Update algorithms to create directories. Now, the static methods return TRUE if the directory could be created or already exists.

--- org.genesez.util/src/org/genesez/util/filesystem/FileSystemHelper.java
+++ org.genesez.util/src/org/genesez/util/filesystem/FileSystemHelper.java
@@ -6,97 +6,56 @@ package org.genesez.util.filesystem;
  * 	@FILE-ID : (_17_0_1_8e00291_1324544960704_472801_1972) 
  */
 
-import java.util.Set;
-import java.nio.file.Path;
 import java.nio.file.Files;
-import java.io.IOException;
-import java.nio.file.attribute.DosFileAttributeView;
-import java.nio.file.attribute.PosixFileAttributeView;
-import java.nio.file.attribute.PosixFilePermissions;
-import java.io.File;
-import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
- * Helper class to create file system elements.
+ * This helper class contains methods that operate on files, directories, or other types of files.
  * 
- * @author nicher
+ * @author	Nico Herbig <nico.herbig@fh-zwickau.de> (maintainer)
  */
 public class FileSystemHelper {
 	
 	// -- generated method stubs for implementations + derived attributes ---
 	/**
-	 * Creates a directory with the specified directory name.
-	 * @param	directoryName	The directory name.
-	 * @return	True if and only if the directory was created, otherwise false.
+	 * Creates a new directory with the specified directory name.
+	 * The {@link #createDirectories(String)} script should be used where it is required to create all nonexistent parent directories first.
+	 * 
+	 * @param	directoryName	The directory to create.
+	 * @return	True if the directory could be created or already exists, otherwise false.
 	 */
 	public static boolean createDirectory(String directoryName) {
 		/* PROTECTED REGION ID(java.implementation._17_0_1_8e00291_1324545009733_326318_2002) ENABLED START */
-		boolean retVal = false;
-		File directory = new File(directoryName);
-		
-		// checks if file (directory) not exists
-		if (!directory.exists()) {
-			// no there is no file or directory which exists.
-			// Try to create directory
-			if (directory.mkdir())
-				retVal = true;
-		}
-		return retVal;
-		/* PROTECTED REGION END */
-	}
-	
-	/**
-	 * Creates a directory with the specified directory path name, including all necessary but nonexistent parent directories.
-	 *
-	 * @param	directoryPathName	The directory path name.
-	 * @return	True if and only if the directory was created, along with all necessary parent directories, otherwise false.
-	 */
-	public static boolean createDirectoryPath(String directoryPathName) {
-		/* PROTECTED REGION ID(java.implementation._17_0_1_8e00291_1324545416605_447454_2019) ENABLED START */
-		boolean retVal = false;
-		File directory = new File(directoryPathName);
-		
-		// checks if file (directory) not exists
-		if (!directory.exists()) {
-			// no there is no file or directory which exists.
-			// Try to create directory
-			if (directory.mkdirs())
-				retVal = true;
-		}
-		return retVal;
-		/* PROTECTED REGION END */
-	}
-	
-	/**
-	 * Changes the file permissions for POSIX or DOS Systems to rwx------ or !readOnly
-	 * 
-	 * @throws IOException if IO-Error occurs.
-	 * @param	file	the path, where permissions will be changed.
-	 * @return	true if successful false if FileSystem is not supported.
-	 * @throws	IOException
-	 */
-	public static boolean alterPermission(Path file) throws IOException {
-		/* PROTECTED REGION ID(java.implementation._17_0_1_2200121_1331731263199_738182_1751) ENABLED START */
-		PosixFileAttributeView POSIXattr = Files.getFileAttributeView(file, PosixFileAttributeView.class);
-		// checks if its a POSIX System
-		if (POSIXattr != null) {
-			Set<PosixFilePermission> perms = PosixFilePermissions.fromString("rwx------");
-			// sets file permissions
-			Files.setPosixFilePermissions(file, perms);
-			return true;
-			
-		} else {
-			// try if its a DOS-like System
+		Path directory = Paths.get(directoryName);
+		if (Files.notExists(directory)) {
 			try {
-				DosFileAttributeView DOSattr = Files.getFileAttributeView(file, DosFileAttributeView.class);
-				// sets file permission
-				DOSattr.setReadOnly(false);
-				return true;
-			} catch (UnsupportedOperationException e) {
-				e.getMessage();
+				Files.createDirectory(directory);
+			} catch (Exception e) {
 				return false;
 			}
 		}
+		return true;
+		/* PROTECTED REGION END */
+	}
+	
+	/**
+	 * Creates a new directory with the specified directory name by creating all nonexistent parent directories first.
+	 *
+	 * @param	directoryName	The directory to create.
+	 * @return	True if the directory could be created along with all parent directories or already exists, otherwise false.
+	 */
+	public static boolean createDirectories(String directoryName) {
+		/* PROTECTED REGION ID(java.implementation._17_0_1_8e00291_1324545416605_447454_2019) ENABLED START */
+		Path directory = Paths.get(directoryName);
+		if (Files.notExists(directory)) {
+			try {
+				Files.createDirectories(directory);
+			} catch (Exception e) {
+				return false;
+			}
+		}
+		return true;
 		/* PROTECTED REGION END */
 	}
 	
