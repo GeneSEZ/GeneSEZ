4250ed0b89ab76c3be6597b08196a862716b06d2
Nico Herbig <nico@nicoherbig.io>
2013-03-01 09:42:35 +0000

    rename tag 1.1.0 to GeneSEZ_1-1-0

---
+++ de.genesez.example/src-gen/base/parts/Screw.java
@@ -0,0 +1,92 @@
+package base.parts;
+
+/* <!-- PROTECTED REGION ID(java.mclassifier.other.import.code_11_5_6340215_1178034133828_951385_16) ENABLED START --> */
+/* <!-- TODO put your imports here --> */
+import java.lang.String;
+
+/* <!-- PROTECTED REGION END --> */
+/**
+ * @author dreamer 
+ */
+public class Screw {
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// generated variable, constant and association declarations
+	// ////////////////////////////////////////////////////////////////////////
+	
+	/** @see {@link getLength} */
+	private float length;
+	private float diameter;
+	private boolean metric;
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// generated constructor implementations
+	// ////////////////////////////////////////////////////////////////////////
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// generated derived attribute method implementations
+	// ////////////////////////////////////////////////////////////////////////
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// generated association method implementations
+	// ////////////////////////////////////////////////////////////////////////
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// generated abstract method declaration
+	// ////////////////////////////////////////////////////////////////////////
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// generated normal method implementations
+	// ////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * @param  count  
+	 */
+	public void screwOn(int count) {
+		
+		/* <!-- PROTECTED REGION ID(java.moperation.implementation.operation.code._12_5_8a7027a_1183038841674_975104_475) ENABLED START --> */
+		/* <!-- TODO put your own implementation code here --> */
+		/* <!-- PROTECTED REGION END --> */
+
+	}
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// generated getter and setter method implementations
+	// ////////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * describes the length of a screw
+	 */
+	public float getLength() {
+		return length;
+	}
+	
+	/** @see {@link getLength} */
+	public void setLength(float _length) {
+		length = _length;
+	}
+	
+	public float getDiameter() {
+		return diameter;
+	}
+	
+	public void setDiameter(float _diameter) {
+		diameter = _diameter;
+	}
+	
+	public boolean getMetric() {
+		return metric;
+	}
+	
+	public void setMetric(boolean _metric) {
+		metric = _metric;
+	}
+	
+	// ////////////////////////////////////////////////////////////////////////
+	// own implementations
+	// ////////////////////////////////////////////////////////////////////////
+	
+	/* <!-- PROTECTED REGION ID(java.mclassifier.implementation.owncode.code._11_5_6340215_1178034133828_951385_16) ENABLED START --> */
+	/* <!-- TODO put your own implementation code here --> */
+	/* <!-- PROTECTED REGION END --> */
+}
