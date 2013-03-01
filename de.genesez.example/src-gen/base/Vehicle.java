284eb648b555913757e0c6f3e10a4f7fa4dd8c8b
Nico Herbig <nico@nicoherbig.io>
2013-03-01 09:42:18 +0000

    rename tag 1.0.0 to GeneSEZ_1-0-0

---
+++ de.genesez.example/src-gen/base/Vehicle.java
@@ -0,0 +1,78 @@
+package base;
+
+
+/* <!-- PROTECTED REGION ID(java.mclassifier.other.import.code_11_5_6340215_1179139182234_646597_164) ENABLED START --> */
+/* <!-- TODO put your imports here --> */
+import java.lang.String;
+
+/* <!-- PROTECTED REGION END --> */
+/**
+ * This abstract class Vehicle is the superclass for all things that can drive (for example: car, boat, air plane, ...).
+ * @author nicher
+ */
+public abstract class Vehicle {
+
+    // ////////////////////////////////////////////////////////////////////////
+    // generated variable, constant and association declarations
+    // ////////////////////////////////////////////////////////////////////////
+    private String make;
+    private VehicleStatus state;
+
+    // ////////////////////////////////////////////////////////////////////////
+    // generated constructor implementations
+    // ////////////////////////////////////////////////////////////////////////
+
+    // ////////////////////////////////////////////////////////////////////////
+    // generated derived attribute method implementations
+    // ////////////////////////////////////////////////////////////////////////
+
+    // ////////////////////////////////////////////////////////////////////////
+    // generated association method implementations
+    // ////////////////////////////////////////////////////////////////////////
+
+    // ////////////////////////////////////////////////////////////////////////
+    // generated abstract method declaration
+    // ////////////////////////////////////////////////////////////////////////
+
+    /**
+     * nothing
+     * @param  speed  define the actual of speed of the vehicle
+     * @return
+     * @throws  WhatAMessException
+     * @throws  NotReallySeriousException  this is a comment; this is the second comment
+     */
+    public abstract boolean drive(int speed)
+        throws WhatAMessException, NotReallySeriousException;
+
+    // ////////////////////////////////////////////////////////////////////////
+    // generated normal method implementations
+    // ////////////////////////////////////////////////////////////////////////
+
+    // ////////////////////////////////////////////////////////////////////////
+    // generated getter and setter method implementations
+    // ////////////////////////////////////////////////////////////////////////
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String _make) {
+        make = _make;
+    }
+
+    public VehicleStatus getState() {
+        return state;
+    }
+
+    public void setState(VehicleStatus _state) {
+        state = _state;
+    }
+
+    // ////////////////////////////////////////////////////////////////////////
+    // own implementations
+    // ////////////////////////////////////////////////////////////////////////
+
+    /* <!-- PROTECTED REGION ID(java.mclassifier.implementation.owncode.code._11_5_6340215_1179139182234_646597_164) ENABLED START --> */
+    /* <!-- TODO put your own implementation code here --> */
+
+    /* <!-- PROTECTED REGION END --> */
+}
--- org.genesez.adapter.ea/README.txt
+++
@@ -1,2 +0,0 @@
-# for local repository jar install
-mvn install:install-file -DgroupId=org.sparx -DartifactId=eaapi -Dpackaging=jar -Dfile=lib/eaapi.jar -DgeneratePom=true -Dversion=1.0.0 
