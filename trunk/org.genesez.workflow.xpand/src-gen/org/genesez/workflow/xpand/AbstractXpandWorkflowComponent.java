e2c1e2bc62a0d25a52af06453ae74bf3be575da8
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2013-03-15 09:40:56 +0000

    revised workflow components to work with transformables, refs #145

--- trunk/org.genesez.workflow.xpand/src-gen/org/genesez/workflow/xpand/AbstractXpandWorkflowComponent.java
+++ trunk/org.genesez.workflow.xpand/src-gen/org/genesez/workflow/xpand/AbstractXpandWorkflowComponent.java
@@ -4,33 +4,41 @@ package org.genesez.workflow.xpand;
  *	Do not place import/include statements above this comment, just below. 
  * 	@FILE-ID : (_Aes3IPqcEeGS1KtqERYUjQ) 
  */
-
 import static org.genesez.workflow.profile.WorkflowFileInclusion.ALWAYS;
 import static org.genesez.workflow.profile.WorkflowFileInclusion.WHEN_NEEDED;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.eclipse.emf.mwe.core.issues.Issues;
 import org.eclipse.xtend.expression.AbstractExpressionsUsingWorkflowComponent.GlobalVarDef;
 import org.eclipse.xtend.typesystem.MetaModel;
 import org.eclipse.xtend.typesystem.emf.EmfMetaModel;
 import org.genesez.workflow.AbstractSlotWorkflowComponent;
-import org.genesez.workflow.profile.Parameter;
+import org.genesez.workflow.profile.WfParameter;
+import org.genesez.workflow.Parameter;
 
 /**
  * Please describe the responsibility of your class in your modeling tool.
  */
 public abstract class AbstractXpandWorkflowComponent extends AbstractSlotWorkflowComponent {
 	
-	@Parameter(isRequired = false, isMultiValued = true, workflowInclusion = ALWAYS)
+	@WfParameter(isRequired = false, isMultiValued = true, workflowInclusion = ALWAYS, isTransformationParameter = true)
 	private java.util.Set<String> aopScript = new java.util.LinkedHashSet<String>();
 	
-	@Parameter(isRequired = false, isMultiValued = true, workflowInclusion = WHEN_NEEDED)
+	@WfParameter(isRequired = false, isMultiValued = true, workflowInclusion = WHEN_NEEDED, isTransformationParameter = true)
 	private java.util.Set<MetaModel> metaModel = new java.util.LinkedHashSet<MetaModel>();
 	
-	@Parameter(isRequired = false, isMultiValued = true, workflowInclusion = WHEN_NEEDED)
+	@WfParameter(isRequired = false, isMultiValued = true, workflowInclusion = WHEN_NEEDED, isTransformationParameter = true)
 	private java.util.Set<GlobalVarDef> globalVarDef = new java.util.LinkedHashSet<GlobalVarDef>();
 	
-	@Parameter(isRequired = false, isMultiValued = true, workflowInclusion = WHEN_NEEDED)
+	@WfParameter(isRequired = false, isMultiValued = true, workflowInclusion = WHEN_NEEDED, isTransformationParameter = true)
 	private java.util.Set<String> emfMetaModelPackage = new java.util.HashSet<String>();
+	
+	@WfParameter(isRequired = false, isMultiValued = false, workflowInclusion = WHEN_NEEDED, isTransformationParameter = true)
+	private String xtendNamingFile;
 	
 	/**
 	 * Validates the configuration of the component before invocation.
@@ -73,6 +81,41 @@ public abstract class AbstractXpandWorkflowComponent extends AbstractSlotWorkflo
 		gvd.setName(name);
 		gvd.setValue(String.valueOf(value));
 		globalVarDef.add(gvd);
+		/* PROTECTED REGION END */
+	}
+	
+	/**
+	 * Method stub for further implementation.
+	 */
+	public void addGlobalVarDefs(java.util.Set<Parameter> parameter) {
+		/* PROTECTED REGION ID(java.implementation._WPosAIl3EeKRAcdOy_Za0Q) ENABLED START */
+		// aggregate multi value parameters
+		Map<String, List<? super Object>> aggr = new LinkedHashMap<String, List<? super Object>>();
+		for (Parameter p : parameter) {
+			String key = p.getName();
+			List<? super Object> value;
+			if (aggr.containsKey(key)) {
+				value = aggr.get(key);
+			} else {
+				value = new ArrayList<>();
+			}
+			value.add(p.getValue());
+			aggr.put(key, value);
+		}
+		// add parameters as global variables
+		for (Map.Entry<String, List<? super Object>> me : aggr.entrySet()) {
+			List<? super Object> values = me.getValue();
+			if (values.size() == 1) {
+				Object o = values.get(0);
+				if (o instanceof String) {
+					addGlobalVarDef(me.getKey(), (String) o);
+				} else {
+					addGlobalVarDef(me.getKey(), o);
+				}
+			} else {
+				addGlobalVarDef(me.getKey(), listToString(values));
+			}
+		}
 		/* PROTECTED REGION END */
 	}
 	
@@ -160,7 +203,21 @@ public abstract class AbstractXpandWorkflowComponent extends AbstractSlotWorkflo
 		this.emfMetaModelPackage.remove(emfMetaModelPackage);
 	}
 	
+	/**
+	 * Returns the value of attribute '<em><b>xtendNamingFile</b></em>'.
+	 */
+	public String getXtendNamingFile() {
+		return xtendNamingFile;
+	}
+	
+	/**
+	 * Sets the value of attribute '<em><b>xtendNamingFile</b></em>'.
+	 * @param	xtendNamingFile	the value to set.
+	 */
+	public void setXtendNamingFile(String xtendNamingFile) {
+		this.xtendNamingFile = xtendNamingFile;
+	}
+	
 	/* PROTECTED REGION ID(java.class.own.code.implementation._Aes3IPqcEeGS1KtqERYUjQ) ENABLED START */
 	/* PROTECTED REGION END */
-	
 }
