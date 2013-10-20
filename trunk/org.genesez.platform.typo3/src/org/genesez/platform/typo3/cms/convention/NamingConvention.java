8fd455545cb24dd403cbd4e1f5a413c8c0724e42
nihe <nihe@a19731f8-c322-0410-8b9b-f98442d78366>
2013-10-20 12:51:55 +0000

    [TASK] Revise NamingConvention.java, #208.

--- trunk/org.genesez.platform.typo3/src/org/genesez/platform/typo3/cms/convention/NamingConvention.java
+++ trunk/org.genesez.platform.typo3/src/org/genesez/platform/typo3/cms/convention/NamingConvention.java
@@ -12,38 +12,45 @@ package org.genesez.platform.typo3.cms.convention;
  * Utility class for TYPO3 CMS naming conventions.
  * 
  * @author	Nico Herbig <nico.herbig@fh-zwickau.de> (maintainer)
+ * TODO		Move functionality to StringProcessing class of org.genesez.util project.
  */
-public class NamingConvention {
+public final class NamingConvention {
 
 	private NamingConvention() {
 	}
-	
+		
 	/**
-	 * Create a TYPO3 CMS extension name by a specified extension key. 
-	 * This method splits a extension key at underscores and transform 
-	 * every token to first upper case to get a valid extension name.
+	 * Create a TYPO3 CMS extension package name by a specified extension key.
+	 * This method splits a extension key at the underscore characters and transform
+	 * every token to first upper case to get a valid extension package name.
 	 * 
 	 * @param	extensionKey	The TYPO3 CMS extension key.
-	 * @return	The TYPO3 CMS extension name based on extension key.
+	 * @return	The TYPO3 CMS extension package name.
 	 */
-	public static String extensionName(String extensionKey) {
-		StringBuffer extensionName = new StringBuffer();
+	public static String extensionPackageName(String extensionKey) {
+		StringBuffer extensionPackageName = new StringBuffer();
 
-		for (String extensionKeyPart : extensionKey.split("_")) {
-			extensionName.append(toUpperCamelCase(extensionKeyPart));
+		for ( String extensionKeyPart : extensionKey.split("_") ) {
+			extensionPackageName.append( toUpperCamelCase(extensionKeyPart) );
 		}
-		return extensionName.toString();
+		return extensionPackageName.toString();
 	}
 
 	/**
-	 * Convert a given string so that the first letter is an capital letter and
-	 * all others are in lower case.
+	 * Convert a given string that the first letter is an capital letter and all others are in lower case.
 	 * 
-	 * @param str The string to convert.
-	 * @return The converted string.
+	 * @param 	string 	The string to convert.
+	 * @return 	The converted string.
 	 */
-	private static String toUpperCamelCase(String str) {
-		return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+	private static String toUpperCamelCase(String string) {
+		String upperCamelCase;
+		
+		if (string.length() <= 1) {
+			upperCamelCase = string.toUpperCase();
+		} else {
+			upperCamelCase = string.substring(0, 1).toUpperCase() + string.substring(1).toLowerCase();
+		}
+		return upperCamelCase;
 	}
 
 }
