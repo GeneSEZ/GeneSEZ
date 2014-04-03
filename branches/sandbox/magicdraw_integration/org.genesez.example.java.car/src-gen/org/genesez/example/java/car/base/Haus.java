b7f3e030844a8fe93d2002622d83b197b46e245a
sew <sew@a19731f8-c322-0410-8b9b-f98442d78366>
2014-04-03 11:09:54 +0000

    Share project "org.genesez.example.java.car" into "svn+ssh://svn.genesez.org/srv/svn/genesez"

---
+++ branches/sandbox/magicdraw_integration/org.genesez.example.java.car/src-gen/org/genesez/example/java/car/base/Haus.java
@@ -0,0 +1,16 @@
+package org.genesez.example.java.car.base;
+
+/* 
+ *	Do not place import/include statements above this comment, just below. 
+ * 	@FILE-ID : (_17_0_4_d550306_1392719254276_85845_4213) 
+ */
+
+/**
+ * Please describe the responsibility of your class in your modeling tool.
+ */
+public class Haus {
+	
+	/* PROTECTED REGION ID(java.class.own.code.implementation._17_0_4_d550306_1392719254276_85845_4213) ENABLED START */
+	// TODO: put your own implementation code here
+	/* PROTECTED REGION END */
+}
